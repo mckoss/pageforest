a638f8a0588c7f22b4fc7d509089d603f216585a
Johann C. Rocholl <johann@rocholl.net>
2010-03-18 13:20:22 -0700

    Added dummy template for home page.

--- appengine/urls.py
+++ appengine/urls.py
@@ -1,17 +1,7 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^appengine/', include('appengine.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^$', direct_to_template, {'template': 'home.html'}),
+    (r'^data/', include('data.urls')),
 )
