b3ee9ad8948bacfc52887cf2884117282fe58617
Johann C. Rocholl <johann@rocholl.net>
2010-03-19 10:48:12 -0700

    Added short comments to unit tests.

--- appengine/dashboard/tests.py
+++ appengine/dashboard/tests.py
@@ -4,10 +4,12 @@ from django.test import TestCase
 class ClientTest(TestCase):
 
     def test_redirect(self):
+        """Append a slash to the URL with permanent redirect."""
         response = self.client.get('/dashboard')
         self.assertRedirects(response, '/dashboard/', status_code=301)
 
     def test_index(self):
+        """Load the dashboard."""
         response = self.client.get('/dashboard/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<title>Dashboard')
