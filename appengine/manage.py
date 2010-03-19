ef220e0d910d80f2345b0fbd484beb43fcbea095
Johann C. Rocholl <johann@rocholl.net>
2010-03-19 10:32:31 -0700

    Print docstring if appengine_django is not installed.

--- appengine/manage.py
+++ appengine/manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 """
-App Engine Helper is required to run this manage.py:
+This version of manage.py requires App Engine Helper:
 http://code.google.com/p/google-app-engine-django/
 
-Install the appengine_django folder somewhere in sys.path, but not in
+Copy the appengine_django folder somewhere in sys.path, but not in
 this application directory, because we don't want to deploy it to App
 Engine for the production system.
 
@@ -14,7 +14,13 @@ appengine_django/tests/serialization_test.py
 """
 
 import os
-import appengine_django
+import sys
+
+try:
+    import appengine_django
+except ImportError:
+    print __doc__
+    sys.exit(1)
 
 # Adjust path because appengine_django is not installed
 # in the application directory.
@@ -25,7 +31,6 @@ from django.core.management import execute_manager
 try:
     import settings  # Assumed to be in the same directory.
 except ImportError:
-    import sys
     sys.stderr.write("""\
 Error: Can't find the file 'settings.py' in the directory containing
 %r. It appears you've customized things. You'll have to run
