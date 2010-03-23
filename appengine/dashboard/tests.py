1b5852cba62c363896e9676ba80e2258d66a043c
Johann C. Rocholl <johann@rocholl.net>
2010-03-22 17:51:15 -0700

    Updated test docstrings for consistency with appengine_django.

--- appengine/dashboard/tests.py
+++ appengine/dashboard/tests.py
@@ -4,12 +4,12 @@ from django.test import TestCase
 class ClientTest(TestCase):
 
     def test_redirect(self):
-        """Append a slash to the URL with permanent redirect."""
+        """Tests that a slash is appended to the URL if required."""
         response = self.client.get('/dashboard')
         self.assertRedirects(response, '/dashboard/', status_code=301)
 
     def test_index(self):
-        """Load the dashboard."""
+        """Tests that the dashboard loads."""
         response = self.client.get('/dashboard/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<title>Dashboard')
--- appengine/data/tests.py
+++ appengine/data/tests.py
@@ -4,26 +4,26 @@ from django.test import TestCase
 class ClientTest(TestCase):
 
     def test_redirect(self):
-        """Append a slash to the URL with permanent redirect."""
+        """Tests that a slash is appended to the URL if required."""
         response = self.client.get('/data')
         self.assertRedirects(response, '/data/', status_code=301)
 
     def test_index(self):
-        """Load data overview page."""
+        """Tests that the data overview page loads."""
         response = self.client.get('/data/')
         self.assertEqual(response.status_code, 200)
 
     def test_demo(self):
-        """Load data demo page."""
+        """Tests that the data demo page loads."""
         response = self.client.get('/data/demo/')
         self.assertEqual(response.status_code, 200)
 
     def test_get_404(self):
-        """GET some data."""
+        """Tests that non-existent data returns 404 Not Found."""
         response = self.client.get('/data/does_not_exist/')
         self.assertEqual(response.status_code, 404)
 
     def test_post_not_allowed(self):
-        """POST request is not allowed."""
+        """Tests that POST request returns 405 Method Not Allowed."""
         response = self.client.post('/data/key')
         self.assertEqual(response.status_code, 405)
