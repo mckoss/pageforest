e5054282ae34418575d48edf3d5f7898a34130f8
Johann C. Rocholl <johann@rocholl.net>
2010-03-24 10:59:20 -0700

    Added simple script to check coding style and unit tests.

---
+++ check.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import subprocess
+
+
+def attempt(command):
+    """
+    Run a shell command and exit with error message if it fails.
+    """
+    print command
+    returncode = subprocess.call(command.split())
+    if returncode:
+        sys.exit("failed with return code %d" % returncode)
+
+
+def main():
+    attempt('pep8 --count --repeat --exclude .hg .')
+    attempt('python appengine/utils/json.py')
+    attempt('python appengine/manage.py test -v0')
+
+
+if __name__ == '__main__':
+    main()
