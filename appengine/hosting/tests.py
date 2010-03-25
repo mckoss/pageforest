1d4cf11ca3a83f152b29385115a9cd4cca73845d
Johann C. Rocholl <johann@rocholl.net>
2010-03-24 17:47:36 -0700

    Simplified HTML content for unit test.

--- appengine/hosting/tests.py
+++ appengine/hosting/tests.py
@@ -25,27 +25,25 @@ class RestApiTest(TestCase):
         key_name = 'test/index.html'
         self.assertEqual(Document.get_by_key_name(key_name), None)
         # Create.
-        response = self.client.put('/index.html', '<html />',
+        response = self.client.put('/index.html', 'html',
                                    content_type='text/html')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, 'saved')
-        self.assertEqual(Document.get_by_key_name(key_name).content,
-                         '<html />')
+        self.assertEqual(Document.get_by_key_name(key_name).content, 'html')
         # Read.
         response = self.client.get('/index.html')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, '<html />')
+        self.assertEqual(response.content, 'html')
         # Read again, with implicit filename.
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, '<html />')
+        self.assertEqual(response.content, 'html')
         # Update.
-        response = self.client.put('/index.html', '<html>updated</html>',
+        response = self.client.put('/index.html', 'updated',
                                    content_type='text/html')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, 'saved')
-        self.assertEqual(Document.get_by_key_name(key_name).content,
-                         '<html>updated</html>')
+        self.assertEqual(Document.get_by_key_name(key_name).content, 'updated')
         # Delete.
         response = self.client.delete('/index.html')
         self.assertEqual(response.status_code, 200)
