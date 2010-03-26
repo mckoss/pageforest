40ef0b490e52a6e01aa86721fd05728513f7e6e2
Johann C. Rocholl <johann@rocholl.net>
2010-03-25 17:05:04 -0700

    Added http_datetime formatter.

--- appengine/utils/http.py
+++ appengine/utils/http.py
@@ -1,4 +1,16 @@
+from calendar import timegm
+from email.Utils import formatdate
+from datetime import datetime
+
 from django.http import HttpResponse
+
+
+def http_datetime(dt):
+    """
+    >>> http_datetime(datetime(1994, 11, 15, 12, 45, 26))
+    'Tue, 15 Nov 1994 12:45:26 GMT'
+    """
+    return formatdate(timegm(dt.utctimetuple()))[:26] + 'GMT'
 
 
 class HttpResponseCreated(HttpResponse):
@@ -7,3 +19,8 @@ class HttpResponseCreated(HttpResponse):
     def __init__(self, location):
         HttpResponse.__init__(self)
         self['Location'] = location
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
