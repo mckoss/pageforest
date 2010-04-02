64c637d08682a097a7255e74c2dae03a85ce74af
Johann C. Rocholl <johann@rocholl.net>
2010-04-02 15:50:48 -0700

    Added list of developers and domains for each app.

--- appengine/apps/models.py
+++ appengine/apps/models.py
@@ -1,21 +1,11 @@
 from google.appengine.ext import db
 
-from auth.models import User
-
 
 class App(db.Model):
     """
-    The name string is stored in app.key().name().
+    The name string is stored in the entity key name.
     """
-    ip = db.StringProperty()
+    domains = db.StringListProperty()  # List of domain names (FQDN).
+    developers = db.StringListProperty()  # List of usernames (see auth.User).
     created = db.DateTimeProperty()
     modified = db.DateTimeProperty()
-
-
-class AppOwner(db.Model):
-    """
-    Many-to-many relationship between App and User model.
-    """
-    app = db.ReferenceProperty(App)
-    admin = db.ReferenceProperty(User)
-    created = db.DateTimeProperty()
