5c308cfcc0299fd0bd7578476144da44e9e29e91
Johann C. Rocholl <johann@rocholl.net>
2010-04-06 19:44:31 -0700

    Added doctest for http_datetime with timezone.

--- appengine/utils/http.py
+++ appengine/utils/http.py
@@ -1,6 +1,6 @@
 from calendar import timegm
 from email.Utils import formatdate
-from datetime import datetime
+from datetime import datetime, timedelta, tzinfo
 
 from django.http import HttpResponse
 
@@ -9,6 +9,12 @@ def http_datetime(dt):
     """
     >>> http_datetime(datetime(1994, 11, 15, 12, 45, 26))
     'Tue, 15 Nov 1994 12:45:26 GMT'
+
+    >>> class PST(tzinfo):
+    ...     def utcoffset(self, dt):
+    ...         return timedelta(minutes=-420)
+    >>> http_datetime(datetime(2010, 12, 31, 20, tzinfo=PST()))
+    'Sat, 01 Jan 2011 03:00:00 GMT'
     """
     return formatdate(timegm(dt.utctimetuple()))[:26] + 'GMT'
 
