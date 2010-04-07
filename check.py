f0f46828ee5cb6cce73aaa5979eef87be967d048
Johann C. Rocholl <johann@rocholl.net>
2010-04-07 13:06:59 -0700

    Redirect stderr to check.log for each check.

--- check.py
+++ check.py
@@ -4,19 +4,29 @@ import os
 import sys
 import subprocess
 
+LOGFILENAME = 'check.log'
+
 
 def attempt(command):
     """
     Run a shell command and exit with error message if it fails.
     """
     print command
-    returncode = subprocess.call(command.split())
+    logfile = open(LOGFILENAME, 'w')
+    returncode = subprocess.call(command.split(), stderr=logfile)
+    logfile.close()
     if returncode:
-        sys.exit("failed with return code %d" % returncode)
+        sys.exit("failed with return code %d, see %s" %
+                 (returncode, LOGFILENAME))
+    else:
+        os.unlink(LOGFILENAME)
 
 
 def main():
     path = os.path.dirname(__file__) or '.'
+    if path != '.':
+        global LOGFILENAME
+        LOGFILENAME = os.path.join(path, LOGFILENAME)
     attempt('pep8 --count --repeat --exclude .hg %s' % path)
     attempt('python %s/pylint.py -e' % path)
     attempt('python %s/whitespace.py' % path)
