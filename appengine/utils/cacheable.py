3a1e0fd095157732277cc09ded216c95a72db5ca
Johann C. Rocholl <johann@rocholl.net>
2010-04-08 12:37:36 -0700

    Cleaned up, now using protocol buffers to serialize model instances.

--- appengine/utils/cacheable.py
+++ appengine/utils/cacheable.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 from google.appengine.ext import db
 from google.appengine.api import memcache
+from google.appengine.datastore import entity_pb
 
 
 class Cacheable(db.Model, object):
@@ -40,6 +41,29 @@ class Cacheable(db.Model, object):
         self._writes_per_second = 0.0
 
     @classmethod
+    def class_get_cache_key(cls, key_name):
+        # TODO: Include settings.CURRENT_VERSION_ID if required.
+        # C1 is a supposedly unique prefix with a version number.
+        # Increase it to reset the cache if the serializer is changed.
+        parts = ('C1', cls.kind(), key_name)
+        return '~'.join(parts)
+
+    def get_cache_key(self):
+        return self.class_get_cache_key(self.key().name())
+
+    @classmethod
+    def cache_get_by_key_name(cls, key_name):
+        cache_key = cls.class_get_cache_key(key_name)
+        binary = memcache.get(cache_key)
+        if binary is None:
+            return None
+        protobuf = entity_pb.EntityProto(binary)
+        instance = db.model_from_protobuf(protobuf)
+        if settings.DEBUG:
+            logging.info("get_by_key_name used cache: " + cache_key)
+        return instance
+
+    @classmethod
     def get_by_key_name(cls, key_name, parent=None):
         """
         Look in local or memcache before datastore.
@@ -49,17 +73,17 @@ class Cacheable(db.Model, object):
         list from datastore for those keys that are not yet cached!
         """
         assert isinstance(key_name, basestring)
-        model = cls._model_from_cache(key_name)
-        if model is not None:
-            return model
+        instance = cls.cache_get_by_key_name(key_name)
+        if instance is not None:
+            return instance
         # Fetch from datastore.
-        model = super(Cacheable, cls).get_by_key_name(key_name, parent)
-        if model is not None:
+        instance = super(Cacheable, cls).get_by_key_name(key_name, parent)
+        if instance is not None:
             if settings.DEBUG:
-                logging.info("datastore get_by_key_name: " +
+                logging.info("get_by_key_name used datastore: " +
                              cls._cache_key(key_name))
-            model.ensure_cached()
-        return model
+            instance.ensure_cached()
+        return instance
 
     @classmethod
     def get_or_insert(cls, key_name, **kwargs):
@@ -68,106 +92,33 @@ class Cacheable(db.Model, object):
         The key_name must be str or unicode.
         """
         assert isinstance(key_name, basestring)
-        model = cls._model_from_cache(key_name)
-        if model is not None:
-            return model
+        instance = cls.cache_get_by_key_name(key_name)
+        if instance is not None:
+            return instance
         # Fetch from datastore.
-        model = super(Cacheable, cls).get_or_insert(key_name, **kwargs)
-        if model is not None:
+        instance = super(Cacheable, cls).get_or_insert(key_name, **kwargs)
+        if instance is not None:
             if settings.DEBUG:
-                logging.info("datastore get_or_insert: " +
-                             cls._cache_key(key_name))
-            model.ensure_cached()
-        return model
+                logging.info("get_or_insert used datastore: " +
+                             cls.class_get_cache_key(key_name))
+            instance.ensure_cached()
+        return instance
+
+    def cache_put(self):
+        """
+        Save this entity to memcache.
+        """
+        cache_key = self.get_cache_key()
+        protobuf = db.model_to_protobuf(self)
+        binary = protobuf.Encode()
+        memcache.set(cache_key, binary)
 
     def put(self):
         """
-        Write to datastore and memcache and local.
+        Save this entitiy to datastore and memcache.
         """
         key = super(Cacheable, self).put()
         if settings.DEBUG:
-            logging.info("datastore put: " + self._model_cache_key())
-        self.ensure_cached()
+            logging.info("put used datastore: " + self.get_cache_key())
+        self.cache_put()
         return key
-
-    def ensure_cached(self):
-        """
-        Ensures that this instance is in the cache. If not, it will
-        replace any existing instance from the cache - so any local
-        modifications will be written to the cache (but not storage).
-
-        Any modifications to the previously cached version will be lost!
-        """
-        model = self._model_from_cache(self.key().name())
-        if model is self:
-            # Not memcached -> write it so available to other instances/threads
-            if not self._is_memcached:
-                self._write_to_cache(self)
-            return
-
-        if model is not None and model._cache_state != self.cache_state.clean:
-            logging.error("Replacing modified model from cache: %s" %
-                          self._model_cache_key())
-
-        self._write_to_cache(self)
-
-    def is_cached(self):
-        model = self._model_from_cache(self.key().name())
-        return model is self
-
-    @classmethod
-    def _model_from_cache(cls, key_name):
-        sKey = cls._cache_key(key_name)
-
-        # Check if in request-local store
-        local_store = cls._local_store()
-        if sKey in local_store:
-            return local_store[sKey]
-
-        # Check if in memcache - and update local store
-        model = memcache.get(sKey)
-        if model is not None:
-            if settings.DEBUG:
-                logging.info("Reading from global cache: %s" % sKey)
-            local_store[sKey] = model
-
-            # Don't copy the cache_state from another instance/request
-            model._cache_state = cls.cache_state.clean
-            model._in_memcache = True
-            return model
-
-        return None
-
-    @staticmethod
-    def _write_to_cache(model):
-        """
-        unconditionally write the model to the local and memcache stores
-        """
-        sKey = model._model_cache_key()
-
-        if settings.DEBUG:
-            logging.info("Writing to cache: %s" % sKey)
-
-        model._local_store()[sKey] = model
-        memcache.set(sKey, model)
-
-        model._is_memcached = True
-
-    def _model_cache_key(self):
-        return self._cache_key(self.key().name())
-
-    @classmethod
-    def _cache_key(cls, key_name):
-        # BUG: cls.__name__ probably not correct for PolyModel classes!
-        return "%s~%s~Cache~%s" % (
-               cls.__name__,
-               key_name,
-               os.environ['CURRENT_VERSION_ID'])
-
-    @staticmethod
-    def _local_store():
-        return local.cache_storage
-
-
-local = threading.local()
-local.cache_storage = {}
