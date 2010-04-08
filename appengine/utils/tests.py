2af55784b9780c8914d5fb33d08f8cdd18c0e523
Johann C. Rocholl <johann@rocholl.net>
2010-04-08 14:25:07 -0700

    Unit test for Cacheable mixin.

--- appengine/utils/tests.py
+++ appengine/utils/tests.py
@@ -1,8 +1,38 @@
 import os
 import imp
 import doctest
+from datetime import datetime
 
 from django.test import TestCase
+
+from google.appengine.ext import db
+from google.appengine.api import memcache
+
+from utils.cacheable import Cacheable
+from utils.mixins import Dated
+
+
+class CachedModel(Cacheable, Dated):
+    text = db.TextProperty()
+    blob = db.BlobProperty()
+
+
+class CacheableTest(TestCase):
+
+    def setUp(self):
+        self.started = datetime.now()
+        self.entity = CachedModel(key_name='e', text='e', blob='e')
+
+    def test_get_by_key_name(self):
+        """Test that get_by_key_name writes the result to memcache."""
+        e1 = CachedModel.cache_get_by_key_name('e')
+        self.assertEqual(e1, None)
+        self.entity.put()
+        e2 = CachedModel.cache_get_by_key_name('e')
+        self.assertEqual(e2.key().name(), 'e')
+        self.entity.delete()
+        e3 = CachedModel.cache_get_by_key_name('e')
+        self.assertEqual(e3, None)
 
 
 class DocTest(TestCase):
