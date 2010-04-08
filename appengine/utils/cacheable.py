eac3228aff0e82014ffe4e5baf2e71cbe3b1481b
Johann C. Rocholl <johann@rocholl.net>
2010-04-08 13:02:36 -0700

    Improved documentation.

--- appengine/utils/cacheable.py
+++ appengine/utils/cacheable.py
@@ -12,40 +12,53 @@ class Cacheable(db.Model, object):
     Memcache mixin for App Engine datastore models.
     Usage: class MyModel(Cacheable)
 
-    This overrides the following Model methods:
-    * get_by_key_name(key_name, parent)
-    * get_or_insert(key_name, **kwargs)
-    * put()
-
-    These are additional methods:
-    * cache_get_by_key_name()
-    * cache_put()
-
-    Deriving from this class provides:
-    * Saving instances to memcache.
+    Deriving from Cacheable class provides:
+    * Automatic memcache writes in instance.put().
+    * Automatic memcache reads in get_by_key_name() or get_or_insert().
     * TODO: Throttled write-through to storage for high-volume writes.
 
-    Datastore queries of this model class will NOT return the cached
-    instances. You should call ensure_cached() to read from the cache.
+    Cacheable overrides the following methods from db.Model:
+    * get_by_key_name(key_name, parent) @classmethod
+    * get_or_insert(key_name, **kwargs) @classmethod
+    * put()
+
+    Cacheable introduces the following new methods:
+    * class_get_cache_key(key_name) @classmethod
+    * get_cache_key()
+    * cache_get_by_key_name()
+    * cache_put()
     """
 
     def __init__(self, *args, **kwargs):
         super(Cacheable, self).__init__(*args, **kwargs)
-        self._writes_per_second = 0.0
 
     @classmethod
     def class_get_cache_key(cls, key_name):
-        # TODO: Include settings.CURRENT_VERSION_ID if required.
-        # C1 is a supposedly unique prefix with a version number.
-        # Increase it to reset the cache if the serializer is changed.
-        parts = ('C1', cls.kind(), key_name)
-        return '~'.join(parts)
+        """
+        Generate a cache key for this key_name.
+
+        C1 is a supposedly unique prefix with a version number.
+        Increase it to reset the cache if the serializer is changed.
+
+        The datastore kind of the class is included to create a
+        separate namespace for each model.
+
+        TODO: Include settings.CURRENT_VERSION_ID if required.
+        """
+        return '~'.join(('C1', cls.kind(), key_name))
 
     def get_cache_key(self):
+        """
+        Generate a cache key for this model instance.
+        """
         return self.class_get_cache_key(self.key().name())
 
     @classmethod
     def cache_get_by_key_name(cls, key_name):
+        """
+        Get a model instance from memcache, using protocol buffers.
+        Return None if the instance was not found in memcache.
+        """
         cache_key = cls.class_get_cache_key(key_name)
         binary = memcache.get(cache_key)
         if binary is None:
@@ -99,7 +112,7 @@ class Cacheable(db.Model, object):
 
     def cache_put(self):
         """
-        Save this entity to memcache.
+        Save this entity to memcache, using protocol buffers.
         """
         cache_key = self.get_cache_key()
         protobuf = db.model_to_protobuf(self)
