d82d60136c6cc36aa082e4d365656488b50cd65a
Johann C. Rocholl <johann@rocholl.net>
2010-04-09 09:41:29 -0700

    Rate limiter for datastore writes.

--- appengine/utils/cacheable.py
+++ appengine/utils/cacheable.py
@@ -1,3 +1,4 @@
+import time
 import logging
 
 from django.conf import settings
@@ -5,6 +6,33 @@ from django.conf import settings
 from google.appengine.ext import db
 from google.appengine.api import memcache
 from google.appengine.datastore import entity_pb
+
+
+class CacheHistory(object):
+
+    def __init__(self, cacheable):
+        self.datastore_put = 0.0  # The beginning of time (1970).
+        self.memcache_puts = []
+        self.cache_key = 'PH~' + cacheable.get_cache_key()
+        data = memcache.get(self.cache_key)
+        if data is None:
+            return
+        timestamps = data.split()
+        if len(timestamps) < 1:
+            return
+        self.datastore_put = float(timestamps.pop(0))
+        self.memcache_puts = [float(t) for t in timestamps]
+
+    def average_put_interval(self):
+        count = len(self.memcache_puts) - 1
+        if count < 3:
+            return 24 * 60 * 60.0  # One day.
+        seconds = self.memcache_puts[-1] - self.memcache_puts[0]
+        return seconds / count
+
+    def serialize(self):
+        timestamps = [self.datastore_put] + self.memcache_puts[-10:]
+        return ' '.join(['%.3f' % t for t in timestamps])
 
 
 class Cacheable(db.Model, object):
@@ -33,17 +61,46 @@ class Cacheable(db.Model, object):
     def __init__(self, *args, **kwargs):
         super(Cacheable, self).__init__(*args, **kwargs)
 
-    def cache_put(self):
-        """Save this entity to memcache, using protocol buffers."""
+    def cache_put(self, history=None):
+        """
+        Save this entity to memcache, using protocol buffers.
+
+        If available, the put history will be saved separately, but
+        using only one memcache call for both entity and history.
+        """
         cache_key = self.get_cache_key()
         protobuf = db.model_to_protobuf(self)
         binary = protobuf.Encode()
-        return memcache.set(cache_key, binary)
+        mapping = {cache_key: binary}
+        if history:
+            mapping[history.cache_key] = history.serialize()
+        return memcache.set_multi(mapping)
 
-    def put(self):
-        """Save this entity to datastore and memcache."""
-        key = super(Cacheable, self).put()
-        self.cache_put()
+    def put(self, commit_interval=1.0):
+        """
+        Save this entity to datastore and memcache.
+
+        If the interval between puts for this entity (in seconds) is
+        consistently smaller than commit_interval, the datastore put
+        will be called only once every commit_interval seconds. If the
+        datastore put is not called, the return value of this method
+        is None instead of the entity key.
+        """
+        key = None
+        history = CacheHistory(self)
+        now = time.time()
+        history.memcache_puts.append(now)
+        if (len(history.memcache_puts) < 4
+            or history.average_put_interval() > commit_interval
+            or now - history.datastore_put > commit_interval):
+            key = super(Cacheable, self).put()
+            # Reload history after slow datastore put.
+            now = time.time()
+            history = CacheHistory(self)
+            history.memcache_puts.append(now)
+            history.datastore_put = now
+        # Save entity and history to memcache.
+        self.cache_put(history)
         return key
 
     def cache_delete(self):
--- appengine/utils/tests.py
+++ appengine/utils/tests.py
@@ -1,6 +1,7 @@
 import os
 import imp
 import doctest
+import time
 from datetime import datetime
 
 from django.test import TestCase
@@ -8,7 +9,7 @@ from django.test import TestCase
 from google.appengine.ext import db
 from google.appengine.api import memcache
 
-from utils.cacheable import Cacheable
+from utils.cacheable import Cacheable, CacheHistory
 from utils.mixins import Dated
 
 
@@ -34,6 +35,41 @@ class CacheableTest(TestCase):
         e3 = CachedModel.cache_get_by_key_name('e')
         self.assertEqual(e3, None)
 
+    def test_write_rate_limit(self):
+        """Test that the datastore put rate is limited."""
+        timestamp = time.time()
+        self.entity.put()  # One.
+        history = CacheHistory(self.entity)
+        self.assertEqual(len(history.memcache_puts), 1)
+        self.assertAlmostEqual(history.datastore_put, timestamp, 2)
+        self.assertAlmostEqual(history.memcache_puts[0], timestamp, 2)
+        self.entity.put()  # Two.
+        self.entity.put()  # Three.
+        self.entity.put()  # Four.
+        self.entity.put()  # Five.
+        history = CacheHistory(self.entity)
+        self.assertEqual(len(history.memcache_puts), 5)
+        self.assertTrue(history.memcache_puts[0] < history.datastore_put)
+        self.assertTrue(history.memcache_puts[1] < history.datastore_put)
+        self.assertEqual(history.memcache_puts[2], history.datastore_put)
+        self.assertTrue(history.memcache_puts[3] > history.datastore_put)
+        self.assertTrue(history.memcache_puts[4] > history.datastore_put)
+        history.datastore_put -= 1.0    # One second earlier.
+        self.entity.cache_put(history)  # Overwrite with fake history.
+        self.entity.put()  # Six, expecting datastore put because outdated.
+        history = CacheHistory(self.entity)
+        self.assertEqual(len(history.memcache_puts), 6)
+        self.assertEqual(history.memcache_puts[5], history.datastore_put)
+        self.entity.put()  # Seven.
+        self.entity.put()  # Eight.
+        self.entity.put()  # Nine.
+        self.entity.put()  # Ten.
+        history = CacheHistory(self.entity)
+        self.assertEqual(len(history.memcache_puts), 10)
+        self.entity.put()  # Eleven.
+        history = CacheHistory(self.entity)
+        self.assertEqual(len(history.memcache_puts), 10)
+
 
 class DocTest(TestCase):
 
@@ -43,6 +79,7 @@ class DocTest(TestCase):
                 return True
 
     def test_utils(self):
+        """Run doctest on utils modules that support it."""
         dir = os.path.dirname(__file__)
         for filename in os.listdir(dir):
             if not filename.endswith('.py'):
