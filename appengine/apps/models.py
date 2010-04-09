5531dc6c5d226b7e61e72897e1aecd74acd94a55
Johann C. Rocholl <johann@rocholl.net>
2010-04-09 16:04:31 -0700

    Added class cache for App model.

--- appengine/apps/models.py
+++ appengine/apps/models.py
@@ -1,11 +1,28 @@
 from google.appengine.ext import db
 
+from utils.mixins import Cacheable, Dated
 
-class App(db.Model):
+
+class App(Cacheable, Dated):
     """
-    The name string is stored in the entity key name.
+    The entity key name contains the app_id string.
     """
-    domains = db.StringListProperty()  # List of domain names (FQDN).
-    developers = db.StringListProperty()  # List of usernames (see auth.User).
-    created = db.DateTimeProperty()
-    modified = db.DateTimeProperty()
+    default_domain = db.StringProperty()  # FQDN.
+    domains = db.StringListProperty()     # FQDN.
+    developers = db.StringListProperty()  # List of usernames.
+
+    @classmethod
+    def get_by_key_name(cls, app_id):
+        """
+        Get app instance from one of these sources:
+        * App.cache (dict in the current process)
+        * Memcache (using Cacheable.get_by_key_name)
+        * Datastore (using db.Model.get_by_key_name)
+        """
+        if not hasattr(App, 'cache'):
+            App.cache = {}
+        if app_id in App.cache:
+            return App.cache[app_id]
+        app = super(App, cls).get_by_key_name(app_id)
+        App.cache[app_id] = app
+        return app
