3fb5f0aa9216dbb1d8d966d5df8627841c906475
Johann C. Rocholl <johann@rocholl.net>
2010-04-12 17:36:05 -0700

    Validate registration form with AJAX.

--- appengine/auth/views.py
+++ appengine/auth/views.py
@@ -1,14 +1,41 @@
+import logging
+
+from django import forms
 from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.utils import simplejson as json
 
 from utils.shortcuts import render_to_response
 
 from auth.forms import RegistrationForm
 
 
-def register(request):
+def form_errors_json(form):
+    if form.is_valid():
+        return '{}'
+    errors = {}
+    fields = {}
+    for fieldname in form.fields:
+        fields[fieldname] = form[fieldname]
+    for key, val in form.errors.iteritems():
+        if key == '__all__':
+            errors[key] == unicode(val[0])
+        elif not isinstance(fields[key].field, forms.FileField):
+            html_id = fields[key].field.widget.attrs.get('id')
+            html_id = html_id or fields[key].auto_id
+            html_id = fields[key].field.widget.id_for_label(html_id)
+            errors[html_id] = unicode(val[0])
+    logging.info(errors)
+    return json.dumps(errors)
+
+
+def register(request, ajax=None):
     """Create a user account on PageForest."""
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
+        if ajax:
+            return HttpResponse(form_errors_json(form),
+                                mimetype='application/json')
         if form.is_valid():
             form.save()
             return redirect('/auth/welcome/')
