eb6d829587fab292ee373008bd24262b1889c8dd
Johann C. Rocholl <johann@rocholl.net>
2010-04-12 17:36:05 -0700

    Validate registration form with AJAX.

--- appengine/auth/urls.py
+++ appengine/auth/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, handler404, handler500
 
 urlpatterns = patterns('auth.views',
-    (r'^register/$', 'register'),
+    (r'^register/(|validate/)$', 'register'),
     (r'^login/$', 'login'),
     (r'^logout/$', 'logout'),
 )
