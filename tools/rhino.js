80b3b126e14b87fc0c01c15f0e9e90f64d0d0951
Johann C. Rocholl <johann@rocholl.net>
2010-04-13 15:42:35 -0700

    Allow multiple filename arguments for jslint.js, so we have to start java only once in jslint.py.

--- tools/rhino.js
+++ tools/rhino.js
@@ -7,30 +7,31 @@
 (function (a) {
     var e, i, input;
     if (!a[0]) {
-        print("Usage: jslint.js file.js");
+        print("Usage: jslint.js file.js ...");
         quit(1);
     }
-    input = readFile(a[0]);
-    if (!input) {
-        print("jslint: Couldn't open file '" + a[0] + "'.");
-        quit(1);
-    }
-    if (!JSLINT(input, {eqeqeq: true, immed: true, newcap: true, nomen: true,
-                        regexp: true, rhino: true, undef: true, white: true,
-                        maxlen: 78})) {
-        for (i = 0; i < JSLINT.errors.length; i += 1) {
-            e = JSLINT.errors[i];
-            if (e) {
-                print(a[0] + ':' + e.line + ':' + e.character +
-                      ': ' + e.reason);
-                // print((e.evidence || '').
-                //     replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
-                print('');
-            }
+    for (var index = 0; index < a.length; index++) {
+        var filename = a[index];
+        input = readFile(filename);
+        if (!input) {
+            print("jslint: Couldn't read " + filename + "");
+            quit(1);
         }
-        quit(2);
-    } else {
-        print("jslint: No problems found in " + a[0]);
-        quit();
+        var options = {eqeqeq: true, immed: true, newcap: true, nomen: true,
+                       regexp: true, rhino: true, undef: true, white: true,
+                       maxlen: 78};
+        if (!JSLINT(input, options)) {
+            for (i = 0; i < JSLINT.errors.length; i += 1) {
+                e = JSLINT.errors[i];
+                if (e) {
+                    print(filename + ':' + e.line + ':' + e.character +
+                          ': ' + e.reason);
+                    // print((e.evidence || '').
+                    //     replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
+                    print('');
+                }
+            }
+            quit(2);
+        }
     }
 }(arguments));
