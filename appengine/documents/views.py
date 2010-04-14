1c82f5175378bb4d10a6c5e6f2b72b82c30defaf
Johann C. Rocholl <johann@rocholl.net>
2010-04-13 22:32:00 -0700

    Initial version of a document view function.

---
+++ appengine/documents/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, Http404
+
+from apps.models import App
+from documents.models import Document
+
+
+def document(request, doc_id):
+    request.app = App.get_by_hostname(request.META.get('HTTP_HOST', 'test'))
+    request.key_name = '/'.join((request.app.key().name(), doc_id))
+    document = Document.get_by_key_name(request.key_name)
+    if document is None:
+        raise Http404
+    return HttpResponse(document.as_json(), mimetype='application/javascript')
