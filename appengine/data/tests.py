ae2fc0d2b152de9b7bbaaae919bff8bb3ea16f98
Johann C. Rocholl <johann@rocholl.net>
2010-04-13 18:32:51 -0700

    Added doc_id in request.key_name, unit tests now passing again.

--- appengine/data/tests.py
+++ appengine/data/tests.py
@@ -107,20 +107,26 @@ class JsonpApiTest(AppTestCase):
 
 class HostTest(AppTestCase):
 
+    def setUp(self):
+        super(TestCase, self).setUp()
+        self.myapp = App(key_name='myapp',
+                         default_domain='myapp.pageforest.com')
+        self.myapp.put()
+
     def test_host(self):
         """Test namespaces by Host header."""
         host_client = Client(HTTP_HOST='myapp.pageforest.com')
-        response = host_client.put('/data/entity', 'data',
-                                   content_type='text/html')
+        url = '/doc_id/key'
+        response = host_client.put(url, 'data', content_type='text/html')
         self.assertContains(response, '"statusText": "Saved"')
-        key_name = 'myapp/data/entity'
+        key_name = 'myapp' + url
         self.assertEqual(KeyValue.get_by_key_name(key_name).value, 'data')
         # GET with the same host header should work.
-        response = host_client.get('/data/entity')
+        response = host_client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, 'data')
         # GET without host header should fail with 404 Not Found.
-        response = self.client.get('/data/entity')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
 
@@ -178,7 +184,7 @@ class MimeTest(AppTestCase):
         response = self.put_and_get('/global/index.html')
         self.assertEqual(response['Content-Type'], 'text/html')
         response = self.client.get('/')
-        self.assertEqual(response['Content-Type'], 'text/html')
+        self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
 
     def test_css(self):
         """Test that the mime type is guessed correctly for CSS."""
@@ -216,7 +222,7 @@ class JsonArrayTest(AppTestCase):
     def setUp(self):
         """Prepare a simple chat array."""
         super(TestCase, self).setUp()
-        self.chat = KeyValue(key_name='test/chat',
+        self.chat = KeyValue(key_name='test/doc/chat',
                              value='["hello", "hi", "howdy"]')
         self.chat.put()
 
@@ -228,45 +234,48 @@ class JsonArrayTest(AppTestCase):
 
     def test_last(self):
         """Test that the end of the array can be retrieved."""
-        self.assertContent('/chat?method=SLICE&start=-1', '["howdy"]')
-        self.assertContent('/chat?method=SLICE&start=-2', '["hi", "howdy"]')
-        self.assertContent('/chat?method=SLICE&start=-3',
+        self.assertContent('/doc/chat?method=SLICE&start=-1',
+                           '["howdy"]')
+        self.assertContent('/doc/chat?method=SLICE&start=-2',
+                           '["hi", "howdy"]')
+        self.assertContent('/doc/chat?method=SLICE&start=-3',
                            '["hello", "hi", "howdy"]')
-        self.assertContent('/chat?method=SLICE&start=-4',
+        self.assertContent('/doc/chat?method=SLICE&start=-4',
                            '["hello", "hi", "howdy"]')
-        self.assertContent('/chat?method=SLICE&start=-999999999999999999',
+        self.assertContent('/doc/chat?method=SLICE&start=-999999999999999999',
                            '["hello", "hi", "howdy"]')
-        self.assertContent('/chat?method=SLICE',
+        self.assertContent('/doc/chat?method=SLICE',
                            '["hello", "hi", "howdy"]')
 
     def test_push(self):
         """Test that push appends to the end af the array."""
         started = datetime.now()
-        response = self.client.post('/chat?method=PUSH', data='bye',
+        response = self.client.post('/doc/chat?method=PUSH', data='bye',
                                     content_type="text/plain")
         self.assertContains(response, '"statusText": "Pushed"')
         self.assertContains(response, '"newLength": 4')
-        self.assertContent('/chat?method=SLICE&start=-2', '["howdy", "bye"]')
-        chat = KeyValue.get_by_key_name('test/chat')
+        self.assertContent('/doc/chat?method=SLICE&start=-2',
+                           '["howdy", "bye"]')
+        chat = KeyValue.get_by_key_name('test/doc/chat')
         self.assertTrue(chat.created <= started)
         self.assertTrue(chat.modified >= started)
 
     def test_push_empty(self):
         """Test that push creates the array if it didn't exist."""
         started = datetime.now()
-        response = self.client.post('/newchat?method=PUSH', data='hi',
+        response = self.client.post('/doc2/chat?method=PUSH', data='hi',
                                     content_type="text/plain")
         self.assertContains(response, '"statusText": "Pushed"')
         self.assertContains(response, '"newLength": 1')
-        self.assertContent('/newchat', '["hi"]')
-        newchat = KeyValue.get_by_key_name('test/newchat')
+        self.assertContent('/doc2/chat', '["hi"]')
+        newchat = KeyValue.get_by_key_name('test/doc2/chat')
         self.assertTrue(newchat.created >= started)
         self.assertTrue(newchat.modified >= started)
 
     def test_push_max(self):
         """Test that push appends to the end af the array."""
-        response = self.client.post('/chat?method=PUSH&max=3', data='bye',
+        response = self.client.post('/doc/chat?method=PUSH&max=3', data='bye',
                                     content_type="text/plain")
         self.assertContains(response, '"statusText": "Pushed"')
         self.assertContains(response, '"newLength": 3')
-        self.assertContent('/chat', '["hi", "howdy", "bye"]')
+        self.assertContent('/doc/chat', '["hi", "howdy", "bye"]')
