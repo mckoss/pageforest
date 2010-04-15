41d240bf23cf5c3d0b374023b99ece67850bf69b
Mike Koss <mckoss@startpad.org>
2010-04-15 16:18:41 -0700

    Fix tabs, import base test.

--- appengine/static/js/base.js
+++ appengine/static/js/base.js
@@ -2,102 +2,102 @@ global_namespace.define('org.startpad.base', function(NS) {
 
 NS.extend(NS, {
 Browser:
-        {
-        version: parseInt(navigator.appVersion),
-        fIE: navigator.appName.indexOf("Microsoft") !== -1
-        },
+    {
+    version: parseInt(navigator.appVersion),
+    fIE: navigator.appName.indexOf("Microsoft") !== -1
+    },
 
 ExtendMissing: function(oDest, var_args)
+    {
+    if (oDest == undefined)
+        oDest = {};
+
+    for (var i = 1; i < arguments.length; i++)
         {
-        if (oDest == undefined)
-                oDest = {};
+        var oSource = arguments[i];
+        for (var prop in oSource)
+            {
+            if (oSource.hasOwnProperty(prop) && oDest[prop] == undefined)
+                oDest[prop] = oSource[prop];
+            }
+        }
 
-        for (var i = 1; i < arguments.length; i++)
-                {
-                var oSource = arguments[i];
-                for (var prop in oSource)
-                        {
-                        if (oSource.hasOwnProperty(prop) && oDest[prop] == undefined)
-                                oDest[prop] = oSource[prop];
-                        }
-                }
-
-        return oDest;
-        },
+    return oDest;
+    },
 
 // Javascript Enumeration
 // Build an object whose properties are mapped to successive integers
 // Also allow setting specific values by passing integers instead of strings.
 // e.g. new NS.Enum("a", "b", "c", 5, "d") -> {a:0, b:1, c:2, d:5}
 Enum: function(aEnum)
-        {
-        if (!aEnum)
-                return;
+    {
+    if (!aEnum)
+        return;
 
-        var j = 0;
-        for (var i = 0; i < aEnum.length; i++)
-                {
-                if (typeof aEnum[i] == "string")
-                        this[aEnum[i]] = j++;
-                else
-                        j = aEnum[i];
-                }
-        },
+    var j = 0;
+    for (var i = 0; i < aEnum.length; i++)
+        {
+        if (typeof aEnum[i] == "string")
+            this[aEnum[i]] = j++;
+        else
+            j = aEnum[i];
+        }
+    },
 
 /* Return new object with just the listed properties "projected" into the new object */
 Project: function(obj, asProps)
-        {
-        var objT = {};
+    {
+    var objT = {};
 
-        for (var i = 0; i < asProps.length; i++)
-                objT[asProps[i]] = obj[asProps[i]];
+    for (var i = 0; i < asProps.length; i++)
+        objT[asProps[i]] = obj[asProps[i]];
 
-        return objT;
-        },
+    return objT;
+    },
 
 DeDupArray: function(a)
-        {
-        if (!a)
-                return;
+    {
+    if (!a)
+        return;
 
-        a.sort();
-        for (var i = 1; i < a.length; i++)
-                {
-                if (a[i-1] == a[i])
-                        a.splice(i, 1);
-                }
-        },
+    a.sort();
+    for (var i = 1; i < a.length; i++)
+        {
+        if (a[i-1] == a[i])
+            a.splice(i, 1);
+        }
+    },
 
 Map: function(a, fn)
-        {
-        var aRes = [];
-        for (var i = 0; i < a.length; i++)
-                aRes.push(fn(a[i]));
-        return aRes;
-        },
+    {
+    var aRes = [];
+    for (var i = 0; i < a.length; i++)
+        aRes.push(fn(a[i]));
+    return aRes;
+    },
 
 Filter: function(a, fn)
+    {
+    var aRes = [];
+    for (var i = 0; i < a.length; i++)
         {
-        var aRes = [];
-        for (var i = 0; i < a.length; i++)
-                {
-                if (fn(a[i]))
-                        aRes.push(a[i]);
-                }
-        return aRes;
-        },
+        if (fn(a[i]))
+            aRes.push(a[i]);
+        }
+    return aRes;
+    },
 
 Reduce: function(a, fn)
-        {
-        if (a.length < 2)
-                return a[0];
+    {
+    if (a.length < 2)
+        return a[0];
 
-        var res = a[0];
-        for (var i = 1; i < a.length-1; i++)
-                res = fn(res, a[i]);
+    var res = a[0];
+    for (var i = 1; i < a.length-1; i++)
+        res = fn(res, a[i]);
 
-        return res;
-        }
+    return res;
+    }
 });
 
 //--------------------------------------------------------------------------
@@ -105,46 +105,46 @@ Reduce: function(a, fn)
 //--------------------------------------------------------------------------
 NS.StBuf = function()
 {
-        this.rgst = [];
-        this.Append.apply(this, arguments);
-        this.sListSep = ", ";
+    this.rgst = [];
+    this.Append.apply(this, arguments);
+    this.sListSep = ", ";
 };
 
 NS.StBuf.prototype = {
-                constructor: NS.StBuf,
+        constructor: NS.StBuf,
 
 Append: function()
-        {
-        for (var ist = 0; ist < arguments.length; ist++)
-                this.rgst.push(arguments[ist].toString());
-        return this;
-        },
+    {
+    for (var ist = 0; ist < arguments.length; ist++)
+        this.rgst.push(arguments[ist].toString());
+    return this;
+    },
 
 Clear: function ()
-        {
-        this.rgst = [];
-        },
+    {
+    this.rgst = [];
+    },
 
 toString: function()
-        {
-        return this.rgst.join("");
-        },
+    {
+    return this.rgst.join("");
+    },
 
 // Build a comma separated list - ignoring undefined, null, empty strings
 AppendList: function()
+    {
+    var sSep = "";
+    for (var ist = 0; ist < arguments.length; ist++)
         {
-        var sSep = "";
-        for (var ist = 0; ist < arguments.length; ist++)
-                {
-                var sT = arguments[ist];
-                if (sT)
-                        {
-                        this.Append(sSep + sT);
-                        sSep = this.sListSep;
-                        }
-                }
-        return this;
+        var sT = arguments[ist];
+        if (sT)
+            {
+            this.Append(sSep + sT);
+            sSep = this.sListSep;
+            }
         }
+    return this;
+    }
 }; // NS.StBuf
 
 //--------------------------------------------------------------------------
@@ -154,36 +154,36 @@ AppendList: function()
 // Wrap a method call in a function
 Function.prototype.FnMethod = function(obj)
 {
-        var _fn = this;
-        return function () { return _fn.apply(obj, arguments); };
+    var _fn = this;
+    return function () { return _fn.apply(obj, arguments); };
 };
 
 // Append additional arguments to a function
 Function.prototype.FnArgs = function()
 {
-        var _fn = this;
-        var _args = [];
+    var _fn = this;
+    var _args = [];
+    for (var i = 0; i < arguments.length; i++)
+        {
+        _args.push(arguments[i]);
+        }
+
+    return function () {
+        var args = [];
+        // In case this is a method call, preserve the "this" variable
+        var self = this;
+
         for (var i = 0; i < arguments.length; i++)
-                {
-                _args.push(arguments[i]);
-                }
+            {
+            args.push(arguments[i]);
+            }
+        for (i = 0; i < _args.length; i++)
+            {
+            args.push(_args[i]);
+            }
 
-        return function () {
-                var args = [];
-                // In case this is a method call, preserve the "this" variable
-                var self = this;
-
-                for (var i = 0; i < arguments.length; i++)
-                        {
-                        args.push(arguments[i]);
-                        }
-                for (i = 0; i < _args.length; i++)
-                        {
-                        args.push(_args[i]);
-                        }
-
-                return _fn.apply(self, args);
-        };
+        return _fn.apply(self, args);
+    };
 };
 
 }); // startpad.base
