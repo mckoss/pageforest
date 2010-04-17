95292af117cb35d5ae40c885050498e0d489facb
Johann C. Rocholl <johann@rocholl.net>
2010-04-16 18:52:26 -0700

    Create dummy request.doc if settings.DEBUG is True.

--- appengine/utils/middleware.py
+++ appengine/utils/middleware.py
@@ -10,31 +10,37 @@ from documents.models import Document
 class AppMiddleware(object):
 
     def process_request(self, request):
+        # Get the app by hostname.
         hostname = request.META.get('HTTP_HOST', 'testserver').split(':')[0]
         if hostname.startswith('www.'):
             hostname = hostname[4:]
         if hostname in (settings.DEFAULT_DOMAIN, 'localhost'):
             return  # Front-end (www.pageforest.com).
-        # Find the app by hostname.
         request.app = App.get_by_hostname(hostname)
-        if request.app is None:
-            app_id = hostname.split('.')[0]
-            request.app = App(key_name=app_id, app_id=app_id)
-        # Rewrite paths with .global to the meta app.
-        logging.info("original path: " + request.path_info)
+        request.app_id = request.app.key().name()
+        # Rewrite / to default HTML page.
+        logging.info("original URL: http://" +
+                     request.META.get('HTTP_HOST', '') +
+                     request.get_full_path())
         if request.path_info == '/':
             request.path_info = '/.global/index.html'
+        # Rewrite /.global/ to the meta app.
         parts = request.path_info.split('/')
         if parts[1] == '.global':
-            parts[1] = request.app.key().name()
+            parts[1] = request.app_id
             request.path_info = '/'.join(parts)
-            request.app = App.get_by_key_name('meta')
+            request.META['HTTP_HOST'] = 'meta'
+            request.app = App.get_by_hostname('meta')
+            request.app_id = request.app.key().name()
         # Prefix path with /app for matching with urls.py.
         request.path_info = '/app' + request.path_info
         request.META['PATH_INFO'] = request.path_info
         request.path = request.META['SCRIPT_NAME'] + request.path_info
-        logging.info("rewritten path: " + request.path_info)
+        logging.info("rewritten URL: http://" +
+                     request.META.get('HTTP_HOST', '') +
+                     request.get_full_path())
         # Load the document.
-        request.app_id = request.app.key().name()
         request.doc_id = parts[1]
         request.doc = Document.get_by_key_name(request.doc_id)
+        if request.doc is None and settings.DEBUG:
+            request.doc = Document(key_name=request.doc_id)
