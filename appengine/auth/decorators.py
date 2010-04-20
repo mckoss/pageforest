37ffb376ced99a6109ad358dea4e7a8ad38081f3
Johann C. Rocholl <johann@rocholl.net>
2010-04-20 12:45:05 -0700

    Let @login_required decorator use request.user from AuthMiddleware.

--- appengine/auth/decorators.py
+++ appengine/auth/decorators.py
@@ -3,8 +3,7 @@ from django.http import HttpResponseForbidden
 
 def login_required(func):
     def wrapper(request, *args, **kwargs):
-        if 'session_key' not in request.COOKIES:
+        if not hasattr(request, 'user'):
             return HttpResponseForbidden("Login is required.")
-        session_key = request.COOKIES['session_key']
         return func(request, *args, **kwargs)
     return wrapper
