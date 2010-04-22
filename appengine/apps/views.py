eba7f8c95ecf89379086d0fdba9efb24d9741cfb
Johann C. Rocholl <johann@rocholl.net>
2010-04-21 18:23:56 -0700

    Separate view function for HTTP PUT.

--- appengine/apps/views.py
+++ appengine/apps/views.py
@@ -5,6 +5,34 @@ from utils.json import model_to_json
 from utils.decorators import jsonp, method_required
 
 
+@method_required('GET', 'PUT')
+def app_info(request):
+    """Read and write application info with REST API."""
+    if request.method == 'GET':
+        content = model_to_json(request.app, exclude='secret'.split())
+        return HttpResponse(content, mimetype='application/json')
+    if request.method == 'PUT':
+        return app_info_put(request)
+
+
+def app_info_put(request):
+    try:
+        parsed = json.loads(request.raw_post_data)
+        for key in ('title', ):
+            if key in parsed:
+                assert_string(key, parsed[key])
+                setattr(request.app, key, parsed[key])
+        for key in ('domains', 'readers', 'writers'):
+            if key in parsed:
+                assert_string_list(key, parsed[key])
+                setattr(request.app, key, parsed[key])
+    except ValueError, e:
+        return HttpResponse(unicode(e), mimetype='text/plain', status=400)
+    request.app.put()
+    return HttpResponse('{"status": 200, "statusText": "Saved"}',
+                        mimetype='application/json')
+
+
 def assert_string(key, value):
     if not isinstance(value, basestring):
         raise ValueError("Expected string value for %s." % key)
@@ -16,26 +44,3 @@ def assert_string_list(key, value):
     for item in value:
         if not isinstance(item, basestring):
             raise ValueError("Expected string values inside %s list." % key)
-
-
-@method_required('GET', 'PUT')
-def app_info(request):
-    """Read and write application info with REST API."""
-    try:
-        if request.method == 'PUT':
-            parsed = json.loads(request.raw_post_data)
-            for key in ('title', ):
-                if key in parsed:
-                    assert_string(key, parsed[key])
-                    setattr(request.app, key, parsed[key])
-            for key in ('domains', 'readers', 'writers'):
-                if key in parsed:
-                    assert_string_list(key, parsed[key])
-                    setattr(request.app, key, parsed[key])
-        request.app.put()
-        return HttpResponse('{"status": 200, "statusText": "Saved"}',
-                            mimetype='application/json')
-    except ValueError, e:
-        return HttpResponse(unicode(e), mimetype='text/plain', status=400)
-    return HttpResponse(model_to_json(request.app, exclude='secret'.split()),
-                        mimetype='application/json')
