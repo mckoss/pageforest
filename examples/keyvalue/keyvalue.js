27171177ae206d4b2d9d194fe1cd7481beae6e76
Johann C. Rocholl <johann@rocholl.net>
2010-04-21 18:03:51 -0700

    Imported simple key-value REST API demo app from bitbucket.

---
+++ examples/keyvalue/keyvalue.js
@@ -0,0 +1,35 @@
+global_namespace.define("com.pageforest.demo", function(ns) {
+
+    function formatResult(xhr, status, message) {
+        return xhr.status + ' ' + xhr.statusText + ' ' +
+            status + ' ' + message + ' ' +
+            xhr.getResponseHeader("X-Echo");
+    }
+
+    function successCallback(message, status, xhr) {
+        $('#results').prepend('<div style="color:#080">' +
+                              formatResult(xhr, status, message) + '</div>');
+    }
+
+    function errorCallback(xhr, status, error) {
+        $('#results').prepend('<div style="color:#800">' +
+                              formatResult(xhr, status, error) + '</div>');
+    }
+
+    function beforeSend(xhr) {
+        xhr.setRequestHeader("X-Hello", "Good morning");
+    }
+
+    ns.ajax = function(method) {
+        options = {
+            type: method,
+            url: '/data/' + $("#id_key").val(),
+            beforeSend: beforeSend,
+            success: successCallback,
+            error: errorCallback,
+        };
+        if (method == "PUT") options.data = $("#id_value").val();
+        $.ajax(options);
+    };
+
+}); // com.pageforest.demo.rest
