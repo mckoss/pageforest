0021f2ba8bc753664e9092e2305b6b2e6403f651
Johann C. Rocholl <johann@rocholl.net>
2010-04-23 10:50:50 -0700

    Updated documentation and removed check_password because the raw password is never transmitted.

--- appengine/auth/models.py
+++ appengine/auth/models.py
@@ -7,10 +7,11 @@ from utils import crypto
 class User(db.Expando, Migratable, Cacheable):
     """
     The entity key name is username.lower() for case-insensitive matching.
+    The password is hmac_sha1(key=raw_password, message=username.lower()).
     """
     username = db.StringProperty()  # May include capital letters.
     email = db.EmailProperty()
-    password = db.StringProperty()  # algo$salt$hexdigest
+    password = db.StringProperty()
     last_login = db.DateTimeProperty(auto_now_add=True)
     date_joined = db.DateTimeProperty(auto_now_add=True)
 
@@ -22,14 +23,7 @@ class User(db.Expando, Migratable, Cacheable):
         Generate HMAC of the username, using the password as secret key.
         This is similar to hashing the password with salt=username.
         """
-        self.password = crypto.hash(self.username.lower(), password)
-
-    def check_password(self, password):
-        """
-        Returns a boolean of whether the password was correct.
-        Handles encryption formats behind the scenes.
-        """
-        return crypto.hash(self.username.lower(), password) == self.password
+        self.password = crypto.hmac_sha1(self.username.lower(), password)
 
     def migrate(self, next_schema):
         """
