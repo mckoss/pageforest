d16c5a543cd7a4d8fe41780d04314364f3a08f45
Johann C. Rocholl <johann@rocholl.net>
2010-04-23 12:09:20 -0700

    Added automatic sha1 and valid_json properties in the KeyValue model.

--- appengine/storage/models.py
+++ appengine/storage/models.py
@@ -1,4 +1,8 @@
+from hashlib import sha1
+
 from google.appengine.ext import db
+
+from django.utils import simplejson as json
 
 from utils.mixins import Cacheable, Migratable, Timestamped
 from apps.models import App
@@ -10,9 +14,24 @@ class KeyValue(Cacheable, Migratable, Timestamped):
     Entity key name format: app_id/doc_id/key/with/slashes
     """
     value = db.BlobProperty()
+    sha1 = db.StringProperty(indexed=False)
+    valid_json = db.BooleanProperty(indexed=False)
 
     def get_absolute_url(self):
         """The URL includes the default domain name for this app."""
         app_id, key = self.key().name().split('/', 1)
         app = App.get_by_key_name(app_id)
         return '/'.join(('http:/', app.domains[0], 'docs', key))
+
+    def put(self):
+        """
+        Update sha1 and valid_json properties automatically before
+        each datastore put.
+        """
+        self.sha1 = sha1(self.value).hexdigest()
+        self.valid_json = True
+        try:
+            json.loads(self.value)
+        except ValueError:
+            self.valid_json = False
+        super(KeyValue, self).put()
