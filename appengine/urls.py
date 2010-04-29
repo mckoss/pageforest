ff79e57305a86fab6a965e4ee6bde7d520adf5e5
Johann C. Rocholl <johann@rocholl.net>
2010-04-29 11:46:09 -0700

    Require trailing slashes in all urls.py files.

--- appengine/urls.py
+++ appengine/urls.py
@@ -3,16 +3,19 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     (r'^$', direct_to_template, {'template': 'home.html'}),
-    (r'^about-us$', direct_to_template, {'template': 'about.html'}),
-    (r'^terms-of-service$', direct_to_template, {'template': 'tos.html'}),
+    (r'^about/$', direct_to_template, {'template': 'about.html'}),
+    (r'^terms-of-service/$', direct_to_template, {'template': 'tos.html'}),
 
+    # Some Django apps have their own urls.py.
     (r'^auth/', include('auth.urls')),
     (r'^dashboard/', include('dashboard.urls')),
 
     # Pageforest applications on subdomains.
     (r'^app/auth/', include('auth.urls')),
-    (r'^app/app.json$', 'apps.views.app_json'),
-    (r'^app/docs/([A-Za-z0-9\._-]*)/?$', 'documents.views.document'),
+    (r'^app/docs/([A-Za-z0-9\._-]*)/$', 'documents.views.document'),
     (r'^app/docs/([A-Za-z0-9\._-]+)/(.+)$', 'storage.views.key_value'),
+
+    # Static hosting for Pageforest apps.
+    (r'^app/app.json$', 'apps.views.app_json'),
     (r'^app/()([A-Za-z0-9\._/-]*)', 'storage.views.key_value'),
 )
