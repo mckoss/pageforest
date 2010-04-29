50e4d8c4b10df82ef35352d69c3a53ac9124fcd9
Johann C. Rocholl <johann@rocholl.net>
2010-04-29 08:57:45 -0700

    Updated dashboard.

--- appengine/dashboard/views.py
+++ appengine/dashboard/views.py
@@ -3,7 +3,14 @@ from datetime import datetime, timedelta
 from utils.shortcuts import render_to_response
 
 from auth.models import User
+from apps.models import App
+from documents.models import Document
 from storage.models import KeyValue
+
+
+def count_recent(model, property, start_times):
+    return [model.all().filter(property + ' >', start_time).count()
+            for start_time in start_times]
 
 
 def index(request):
@@ -11,15 +18,20 @@ def index(request):
     Dashboard overview.
     """
     now = datetime.now()
-    day_ago = now - timedelta(hours=24)
-    week_ago = now - timedelta(days=7)
-    month_ago = now - timedelta(days=30)
-    new_users = (
-        User.all().filter('joined >', day_ago).count(),
-        User.all().filter('joined >', week_ago).count(),
-        User.all().filter('joined >', month_ago).count())
-    new_data = (
-        KeyValue.all().filter('timestamp >', day_ago).count(),
-        KeyValue.all().filter('timestamp >', week_ago).count(),
-        KeyValue.all().filter('timestamp >', month_ago).count())
-    return render_to_response(request, 'dashboard/index.html', locals())
+    start_times = [
+        now - timedelta(hours=24),
+        now - timedelta(days=7),
+        now - timedelta(days=30),
+        ]
+    keyvalue_created = count_recent(KeyValue, 'created', start_times)
+    keyvalue_modified = count_recent(KeyValue, 'modified', start_times)
+    for index in range(len(keyvalue_modified)):
+        keyvalue_modified[index] -= keyvalue_created[index]
+    dictionary = {
+        'user_joined': count_recent(User, 'date_joined', start_times),
+        'app_created': count_recent(App, 'created', start_times),
+        'document_created': count_recent(Document, 'created', start_times),
+        'keyvalue_created': keyvalue_created,
+        'keyvalue_modified': keyvalue_modified,
+        }
+    return render_to_response(request, 'dashboard/index.html', dictionary)
