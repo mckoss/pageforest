10a538640fecdfadbc275e6005fc379a37732df4
Johann C. Rocholl <johann@rocholl.net>
2010-04-29 11:46:09 -0700

    Require trailing slashes in all urls.py files.

--- appengine/auth/urls.py
+++ appengine/auth/urls.py
@@ -3,18 +3,14 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('auth.views',
     # User interface
-    (r'^sign-up$', 'register'),
-    # AJAX validator for user registration.
-    (r'^welcome$', direct_to_template,
-        {'template': 'auth/welcome.html'}),
-
-    # Optional token.
-    (r'^sign-in$', 'sign_in'),
-    (r'^sign-out$', 'sign_out'),
+    (r'^sign-up/$', 'register'),
+    (r'^sign-in/$', 'sign_in'),
+    (r'^sign-out/$', 'sign_out'),
+    (r'^welcome/$', direct_to_template, {'template': 'auth/welcome.html'}),
 
     # Auth API for http://app_id.pageforest.com/auth
-    (r'^reauth$', 'reauth'),
-    (r'^challenge$', 'challenge'),
-    (r'^verify/(.+)$', 'verify'),
-    (r'^poll/(.+)$', 'poll'),  # With token.
+    (r'^reauth/$', 'reauth'),
+    (r'^challenge/$', 'challenge'),
+    (r'^verify/(.+)/$', 'verify'),
+    (r'^poll/(.+)/$', 'poll'),  # With token.
 )
