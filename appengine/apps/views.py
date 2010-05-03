cfffccfc4db3066edce8ffb23b7d42ad463a7ac4
Johann C. Rocholl <johann@rocholl.net>
2010-05-02 23:23:40 -0700

    Added docstrings, and @jsonp decorator for app_json view function.

--- appengine/apps/views.py
+++ appengine/apps/views.py
@@ -5,7 +5,7 @@ from utils.json import model_to_json
 from utils.decorators import jsonp, method_required
 
 
-# TODO: @jsonp?
+@jsonp
 @method_required('GET', 'PUT')
 def app_json(request):
     """Read and write application info with REST API."""
@@ -16,7 +16,6 @@ def app_json(request):
         return app_json_put(request)
 
 
-# TODO: @jsonp?
 def app_json_put(request):
     try:
         parsed = json.loads(request.raw_post_data)
@@ -38,11 +37,17 @@ def app_json_put(request):
 
 
 def assert_string(key, value):
+    """
+    Check that the value is a string.
+    """
     if not isinstance(value, basestring):
         raise ValueError("Expected string value for %s." % key)
 
 
 def assert_string_list(key, value):
+    """
+    Check that the value is a list of strings.
+    """
     if not isinstance(value, list):
         raise ValueError("Expected string list for %s." % key)
     for item in value:
