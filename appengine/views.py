3a3e0a98d9012a038fd03249182e716b94098869
Johann C. Rocholl <johann@rocholl.net>
2010-05-02 23:26:22 -0700

    Added missing blank line before function definition.

--- appengine/views.py
+++ appengine/views.py
@@ -8,6 +8,7 @@ from utils.decorators import method_required
 # REVIEW: It seems unclean to have a views.py in the top level.
 # Should we move this to appengine.utils or similar?
 
+
 @method_required('GET')
 def reserved(request):
     return HttpResponseNotFound("This url reserved for future use of %s." %
