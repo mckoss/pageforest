07f4d18b324b801cbaadc54dca9134fd0b4389c2
Johann C. Rocholl <johann@rocholl.net>
2010-05-03 15:50:16 -0700

    Cleaned up and improved inline documentation.

--- appengine/urls.py
+++ appengine/urls.py
@@ -3,16 +3,19 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns(
     '',
+
+    # Static pages.
     (r'^$', direct_to_template, {'template': 'home.html'}),
     (r'^about/$', direct_to_template, {'template': 'about.html'}),
     (r'^terms-of-service/$', direct_to_template, {'template': 'tos.html'}),
 
+    # Include separate urls.py for Django apps.
     (r'^auth/', include('auth.urls')),
     (r'^dashboard/', include('dashboard.urls')),
 
     # Pageforest applications on subdomains.
     # REVIEW: I don't think we want the same urls in pf.com/auth and in
-    # app.pf.com/auth
+    # app.pf.com/auth -- JCR: I agree.
     (r'^app/auth/', include('auth.urls')),
 
     # REVIEW: Don't repeat settings.DOC_ID_RE here (DRY).
@@ -20,12 +23,11 @@ urlpatterns = patterns(
     (r'^app/docs/(?P<doc_id>[A-Za-z0-9\._-]+)/(?P<key>.+)$',
      'storage.views.key_value'),
 
-    # Static hosting for Pageforest apps.
-    (r'^app/app.json$', 'apps.views.app_json'),
-
     # Application keyspace reserved for future use
     (r'^app/(docs|data)/', 'utils.views.reserved'),
 
-    # REVIEW: What's the empty pattern for?
-    (r'^app/()([A-Za-z0-9\._/-]*)', 'storage.views.key_value'),
+    # Static hosting for Pageforest apps.
+    (r'^app/app.json$', 'apps.views.app_json'),
+    (r'^app/(?P<doc_id>)(?P<key>[A-Za-z0-9\._/-]*)',
+     'storage.views.key_value'),
 )
