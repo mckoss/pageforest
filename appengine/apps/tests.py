ac6a0cb8e010c01b95c1f8c626c79d6dac99fd22
Johann C. Rocholl <johann@rocholl.net>
2010-05-02 18:52:57 -0700

    Removed doctest.testmod() because we can't run models.py on the command line, and because Django models are automatically integrated with doctest.

--- appengine/apps/tests.py
+++ appengine/apps/tests.py
@@ -32,27 +32,3 @@ class HostnameTest(TestCase):
         self.assertFalse(app.is_saved())
         self.assertEqual(app.title, 'Unknown')
         self.assertEqual(app.domains, ['unknown.pageforest.com'])
-
-
-class DocTest(TestCase):
-
-    def has_doctest_testmod(self, filename):
-        """Check if doctest.testmod() appears in the file."""
-        for line in file(filename):
-            if line.strip() == 'doctest.testmod()':
-                return True
-
-    def test_utils(self):
-        """Run doctest on utils modules that support it."""
-        dir = os.path.dirname(__file__)
-        for filename in os.listdir(dir):
-            if not filename.endswith('.py'):
-                continue
-            full_path = os.path.join(dir, filename)
-            if not self.has_doctest_testmod(full_path):
-                continue
-            base, ext = os.path.splitext(filename)
-            file, pathname, desc = imp.find_module(base, [dir])
-            mod = imp.load_module('apps.' + base, file, pathname, desc)
-            failures, tests = doctest.testmod(mod)
-            self.assertEqual(failures, 0)
