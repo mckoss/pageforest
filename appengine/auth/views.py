0e0b473275e4e312cc0bab755ffbdecc5e102c4f
Johann C. Rocholl <johann@rocholl.net>
2010-05-06 10:41:31 -0700

    Changed default character set for crypto.random64() to BASE64URL and removed crypto.random64url().

--- appengine/auth/views.py
+++ appengine/auth/views.py
@@ -134,7 +134,7 @@ def challenge(request):
 
     Challenge is S(random/expires/ip, app.secret)
     """
-    random_key = crypto.random64url(32)
+    random_key = crypto.random64(32)
     expires = int(time.time() + CHALLENGE_EXPIRATION)
     ip = request.META.get('REMOTE_ADDR', '0.0.0.0')
     challenge_string = crypto.sign(random_key, expires, ip, request.app.secret)
