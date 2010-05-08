976209ad03527d40a7480d87e24e6be027c764c9
Johann C. Rocholl <johann@rocholl.net>
2010-05-07 17:38:36 -0700

    Simplified.

--- appengine/skeleton/main.js
+++ appengine/skeleton/main.js
@@ -1,41 +1,34 @@
 global_namespace.define('com.pageforest.skeleton', function (ns) {
 
-    ns.ready = function () {
+    ns.ready = function() {
         $('#title').focus();
     };
 
-    ns.signin = function () {
+    ns.signin = function() {
         var dot = location.host.indexOf('.');
         var www = "www" + location.host.substr(dot);
         var url = "http://" + www + "/sign-in/skeleton/";
         var tab = window.open(url, '_blank');
     };
 
-    ns.save = function () {
-        var url = '/docs/' + $('#name').val();
-        var data = JSON.stringify({
-            title: $('#title').val(),
-            blob: JSON.parse($('#content').val())
-        });
+    ns.save = function() {
+        var data = '{"title": "' + $('#title').val() + '",' +
+            '"blob": ' + $('#content').val() + '}';
         $.ajax({
             type: 'PUT',
-            url: url,
-            data: data,
-            contentType: 'application/json'
+            url: '/docs/' + $('#name').val(),
+            data: data
         });
     };
 
-    ns.load = function () {
-        var url = '/docs/' + $('#name').val();
+    ns.load = function() {
         $.ajax({
-            url: url,
-            success: ns.success
+            url: '/docs/' + $('#name').val(),
+            success: function(document) {
+                $('#title').val(document.title);
+                $('#content').val(document.blob);
+            }
         });
-    };
-
-    ns.success = function (message, status, xhr) {
-        $('#title').val(message.title);
-        $('#content').val(message.blob);
     };
 
 });
