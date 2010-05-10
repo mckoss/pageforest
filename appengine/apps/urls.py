7c9385325e4e45bed40536f023a26de5e155ed1a
Johann C. Rocholl <johann@rocholl.net>
2010-05-10 10:56:09 -0700

    Removed obsolete create_app view.

--- appengine/apps/urls.py
+++ appengine/apps/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls.defaults import patterns, handler404, handler500
 
 urlpatterns = patterns(
     'apps.views',
-    (r'^(?P<app_id>%s)/$' % settings.APP_ID_REGEX, 'create_app'),
     (r'^(?P<app_id>%s)/app.json$' % settings.APP_ID_REGEX, 'app_json'),
 )
