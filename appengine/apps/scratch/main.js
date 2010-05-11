cba3de246794a2bf46e7f65566b59162766ed328
Mike Koss <mckoss@startpad.org>
2010-05-10 17:26:02 -0700

    Working on scratch application example.

--- appengine/apps/scratch/main.js
+++ appengine/apps/scratch/main.js
@@ -19,23 +19,40 @@ global_namespace.define('com.pageforest.scratch', function (ns) {
     ns.save = function() {
         var data = JSON.stringify({
             title: $('#title').val(),
-            blob: $('#blob').val()
+            blob: $('#blob').val(),
+            readers: ['public']
         });
         $.ajax({
             type: 'PUT',
             url: '/docs/' + $('#name').val(),
-            data: data
+            data: data,
+            complete: ns.showStatus,
+            success: function(data) {
+                alert(JSON.stringify(data, undefined, 4));
+            }
         });
     };
 
     ns.load = function() {
         $.ajax({
             url: '/docs/' + $('#name').val(),
+            complete: ns.showStatus,
             success: function(document) {
                 $('#title').val(document.title);
                 $('#blob').val(document.blob);
             }
         });
+    };
+
+    ns.showStatus = function(ajax, status) {
+        var now = new Date();
+        var time = now.getHours() + ':' +
+            (100 + now.getMinutes()).toString().substr(1) + ':' +
+            (100 + now.getSeconds()).toString().substr(1);
+        message = '<div>' + time + ': ' + status;
+        message += ' (' + ajax.status + ')';
+        message += '</div>';
+        $('#results').prepend(message);
     };
 
 });
