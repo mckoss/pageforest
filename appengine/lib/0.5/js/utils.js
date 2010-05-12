1ec1f06e9cb7e571551728ae061857413f4ba419
Mike Koss <mckoss@startpad.org>
2010-05-11 22:37:14 -0700

    Update built files.

--- appengine/lib/0.5/js/utils.js
+++ appengine/lib/0.5/js/utils.js
@@ -1,168 +1,231 @@
 /* Begin file: namespace.js */
 /* Namespace.js
 
-   Version 1.0, June 2009
+   Version 2.0, May 11, 2010
    by Mike Koss - released into the public domain.
 
    Support for building modular namespaces in javascript.
 
    Globals:
 
-   window.global_namespace (Namespace) - The top of the namespace hierarchy.
-   Child namespaces are stored as properties in each namespace object.
+   namespace - The top of the namespace hierarchy. Child
+   namespaces are stored as properties in each namespace object.
+
+   namespace.lookup(path) - Return the namespace object with the given
+   path. Creates the namespace if it does not already exist. The path
+   has the form ('unique.module.sub_module').
+
+   Utility functions:
+
+   util = namespace.util;
+
+   util.isOwn(object, property) - Test if hasOwnProperty for for..in
+   loops.
+
+   util.extendObject(dest, source1, source2, ...) - Copy the properties
+   from the sources into the destination (properties in following objects
+   override those from the preceding objects).
 
    *** Class Namespace ***
 
    Methods:
 
-   ns.define(sPath, fnCallback(ns)) - Define a new Namespace object
-   and call the provided function with the new namespace as a
-   parameter. Returns the newly defined namespace. sPath has the form
-   ('unique.module.sub_module').
+   ns.define(callback(ns)) - Call the provided function with the new
+   namespace as a parameter. Returns the newly defined namespace.
 
-   ns.extend(oDest, oSource) - Copy the (own) properties of the source
-   object into the destination object. Returns oDest. Note: This
-   method is a convenience function - it has no effect on the
-   Namespace object itself.
+   ns.defineOnce(callback(ns)) - Same as 'define', but only allows the
+   first invocation of the callback function.
 
-   ns.import(sPath) - Return the namespace object with the given path.
+   ns.extend(object) - Copy the (own) properties of the source
+   object into the namespace.
+
+   ns.nameOf(symbol) - Return the global name of a symbol in a namespace
+   (for eval() or html onEvent attributes).
 
    Usage example:
 
-   global_namespace.define('org.startpad.base', function(ns) {
-       var Other = ns.Import('org.startpad.other');
+   namespace.lookup('org.startpad.base').define(function(ns) {
+       var util = namespace.util;
+       var other = ns.lookup('org.startpad.other');
 
-       ns.extend(ns, {
+       ns.extend({
            var1: value1,
            var2: value2,
            myFunc: function(args) {
-               ...Other.AFunction(args)...
+               ...other.aFunction(args)...
            }
        });
 
+       // Constructor
        ns.ClassName = function(args) {
+           ...
        };
 
-       ns.ClassName.prototype = {
-           constructor: ns.ClassName,
+       util.extendObject(ns.ClassName.prototype, {
            var1: value1,
 
            method1: function(args) {
            }
-       };
+       });
    });
 */
 
 // Define stubs for FireBug objects if not present.
 // This is here because this will often be the first javascript file loaded.
-if (!window.console) {
-    (function () {
+// We refrain from using the window object as we may be in a web worker where
+// the global scope is NOT window.
+try {
+    var console = (function () {
+        if (console != undefined)
+            return console;
         var noop = function () {};
         var names = ["log", "debug", "info", "warn", "error", "assert",
                      "dir", "dirxml", "group", "groupEnd", "time", "timeEnd",
                      "count", "trace", "profile", "profileEnd"];
-        window.console = {};
+
+        var consoleT = {};
         for (var i = 0; i < names.length; ++i) {
-            window.console[names[i]] = noop;
+            consoleT[names[i]] = noop;
         }
+
+        return consoleT;
     }());
 }
+catch (e) {}
 
-(function () {
-    var sGlobal = 'global_namespace';
-
-    // Don't run this function more than once.
-    if (window[sGlobal]) {
-        return;
+var namespace = (function () {
+    try {
+        if (namespace != undefined)
+            return namespace;
     }
+    catch (e) {}
 
-    /** @constructor **/
-    function Namespace(nsParent, sName) {
-        if (sName) {
-            sName = sName.replace(/-/g, '_');
+    function Namespace(parent, name) {
+        if (name) {
+            name = name.replace(/-/g, '_');
         }
 
-        this._nsParent = nsParent;
+        this._isDefined = false;
+        this._parent = parent;
 
-        if (this._nsParent) {
-            this._nsParent[sName] = this;
-            this._sPath = this._nsParent._sPath;
-            if (this._sPath !== '') {
-                this._sPath += '.';
+        if (this._parent) {
+            this._parent[name] = this;
+            this._path = this._parent._path;
+            if (this._path !== '') {
+                this._path += '.';
             }
-            this._sPath += sName;
+            this._path += name;
         } else {
-            this._sPath = '';
+            this._path = '';
         }
     }
 
-    Namespace.prototype.extend = function (oDest, var_args) {
-        if (oDest === undefined) {
-            oDest = {};
+    var namespaceT = new Namespace(null);
+
+    // Extend an object's properties from one (or more) additional
+    // objects.
+    function extendObject(dest, args) {
+        if (dest === undefined) {
+            dest = {};
         }
 
         for (var i = 1; i < arguments.length; i++) {
-            var oSource = arguments[i];
-            for (var prop in oSource) {
-                if (oSource.hasOwnProperty(prop)) {
-                    oDest[prop] = oSource[prop];
+            var source = arguments[i];
+            for (var prop in source) {
+                if (source.hasOwnProperty(prop)) {
+                    dest[prop] = source[prop];
                 }
             }
         }
+        return dest;
+    }
 
-        return oDest;
-    };
-
-    var ns = window[sGlobal] = new Namespace(null);
-
-    ns.extend(Namespace.prototype, {
-        define: function (sPath, fnCallback) {
-            sPath = sPath.replace(/-/g, '_');
-
-            var aPath = sPath.split('.');
-            var nsCur = this;
-            for (var i = 0; i < aPath.length; i++) {
-                var sName = aPath[i];
-                if (nsCur[sName] === undefined) {
-                    var nsNew = new Namespace(nsCur, sName);
-                }
-                nsCur = nsCur[sName];
+    // Functions added to every Namespace.
+    extendObject(Namespace.prototype, {
+        // Call a function with the namespace as a parameter - forming
+        // a closure for the namespace definition.
+        define: function(callback) {
+            this._isDefined = true;
+            console.info("Namespace '" + this._path + "' defined.");
+            if (callback) {
+                callback(this);
             }
+            return this;
+        },
+
+        // Same as define, but will not execute the callback more than once.
+        defineOnce: function(callback) {
             // In case a namespace is multiply loaded, we ignore the
             // definition function for all but the first call.
-            if (fnCallback) {
-                if (!nsCur._fDefined) {
-                    nsCur._fDefined = true;
-                    fnCallback(nsCur);
-                     console.info("Namespace '" + nsCur._sPath +
-                                  "' defined.");
-                } else {
-                    console.warn("WARNING: Namespace '" + nsCur._sPath +
-                                 "' redefinition.");
-                }
-            } else if (!nsCur._fDefined) {
-                console.warn("Namespace '" + nsCur._sPath +
-                             "' forward reference.");
+            if (this._isDefined) {
+                console.warn("WARNING: Namespace '" + this._path +
+                             "' redefinition.");
+                return this;
             }
-            return nsCur;
+
+            return this.define(callback);
         },
 
-        lookup: function (sPath) {
-            return window[sGlobal].define(sPath);
+        // Extend the namespace from the arguments of this function.
+        extend: function() {
+            args = [this];
+            for (var i = 0; i < arguments.length; i++) {
+                args.push(arguments[i]);
+            }
+            return extendObject.apply(undefined, args);
         },
 
-        nameOf: function (sInNamespace) {
-            sInNamespace = sInNamespace.replace(/-/g, '_');
-            return sGlobal + '.' + this._sPath + '.' + sInNamespace;
-        },
+        // Return a global name for a namespace symbol (for eval()
+        // or use in onEvent html attributes.
+        nameOf: function(symbol) {
+            symbol = symbol.replace(/-/g, '_');
+            return 'namespace.' + this._sPath + '.' + symbol;
+        }
 
-        isOwn: function (object, name) {
-            return Object.prototype.hasOwnProperty.call(object, name);
+    });
+
+    // Functions added to the top level namespace (only).
+    extendObject(namespaceT, {
+        // Lookup a global namespace object, creating it (and it's parents)
+        // as necessary.
+        lookup: function(path) {
+            path = path.replace(/-/g, '_');
+            var parts = path.split('.');
+            var cur = namespaceT;
+            for (var i = 0; i < parts.length; i++) {
+                var name = parts[i];
+                if (cur[name] === undefined) {
+                    cur = new Namespace(cur, name);
+                }
+                else {
+                    cur = cur[name];
+                }
+            }
+            return cur;
         }
     });
+
+    // Put utilities in the 'util' namespace beneath the root.
+    namespaceT.lookup('util').extend({
+        // Helper for "for..in" loops like so:
+        // for (prop in object) {
+        //     if (namespace.isOwn(object, prop)) {
+        //     ...
+        //     }
+        // }
+        isOwn: function(object, name) {
+            return Object.prototype.hasOwnProperty.call(object, name);
+        },
+
+        extendObject: function() {
+            return extendObject(arguments);
+        }
+    }).defineOnce();
+
+    return namespaceT;
 }());
 /* Begin file: misc.js */
-global_namespace.define("com.pageforest.misc", function(ns) {
+namespace.lookup("com.pageforest.misc").define(function(ns) {
 
     ns.strip = function(s) {
         return (s || "").replace(/^\s+|\s+$/g, "");
@@ -170,7 +233,7 @@ global_namespace.define("com.pageforest.misc", function(ns) {
 
 });
 /* Begin file: random.js */
-global_namespace.define("com.pageforest.random", function(ns) {
+namespace.lookup("com.pageforest.random").defineOnce(function(ns) {
 
     ns.upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     ns.lower = 'abcdefghijklmnopqrstuvwxyz';
@@ -193,7 +256,7 @@ global_namespace.define("com.pageforest.random", function(ns) {
 
 }); // com.pageforest.random
 /* Begin file: cookies.js */
-global_namespace.define('com.pageforest.cookies', function(ns) {
+namespace.lookup('com.pageforest.cookies').define(function(ns) {
     /*
     Client-side cookie reader and writing helper.
 
@@ -202,44 +265,44 @@ global_namespace.define('com.pageforest.cookies', function(ns) {
     character These routines use encodeURIComponent to safely encode
     and decode all special characters.
     */
-    var misc = ns.lookup('com.pageforest.misc');
+    var misc = namespace.lookup('com.pageforest.misc');
 
-ns.extend(ns, {
-setCookie: function(name, value, days, path) {
-    var expires = '';
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        expires = '; expires=' + date.toGMTString();
-    }
-    path = '; path=' + (path || '/');
-    document.cookie =
-        encodeURIComponent(name) + '=' + encodeURIComponent(value)
-        + expires + path;
-},
+    ns.extend({
+    setCookie: function(name, value, days, path) {
+        var expires = '';
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+            expires = '; expires=' + date.toGMTString();
+        }
+        path = '; path=' + (path || '/');
+        document.cookie =
+            encodeURIComponent(name) + '=' + encodeURIComponent(value)
+            + expires + path;
+    },
 
-getCookie: function(name) {
-    return ns.getCookies()[name];
-},
+    getCookie: function(name) {
+        return ns.getCookies()[name];
+    },
 
-getCookies: function(name) {
-    var st = document.cookie;
-    var rgPairs = st.split(";");
+    getCookies: function(name) {
+        var st = document.cookie;
+        var rgPairs = st.split(";");
 
-    var obj = {};
-    for (var i = 0; i < rgPairs.length; i++) {
-        // document.cookie never returns ;max-age, ;secure, etc. - just name value pairs
-        rgPairs[i] = misc.strip(rgPairs[i]);
-        var rgC = rgPairs[i].split("=");
-        var val = decodeURIComponent(rgC[1]);
-        // Remove quotes around value string if any (and also replaces \" with ")
-        var rg = val.match('^"(.*)"$');
-        if (rg)
-            val = rg[1].replace('\\"', '"');
-        obj[decodeURIComponent(rgC[0])] = val;
-    }
-    return obj;
+        var obj = {};
+        for (var i = 0; i < rgPairs.length; i++) {
+            // document.cookie never returns ;max-age, ;secure, etc. - just name value pairs
+            rgPairs[i] = misc.strip(rgPairs[i]);
+            var rgC = rgPairs[i].split("=");
+            var val = decodeURIComponent(rgC[1]);
+            // Remove quotes around value string if any (and also replaces \" with ")
+            var rg = val.match('^"(.*)"$');
+            if (rg)
+                val = rg[1].replace('\\"', '"');
+            obj[decodeURIComponent(rgC[0])] = val;
+        }
+        return obj;
 
-}}); // ns
+    }}); // ns
 
 }); // com.pageforest.cookies
