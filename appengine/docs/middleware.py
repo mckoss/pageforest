b8b94726f7bdc7ba2495a22a196720df07e6f488
Johann C. Rocholl <johann@rocholl.net>
2010-05-12 12:13:31 -0700

    Code review and cleanup with Mike.

--- appengine/docs/middleware.py
+++ appengine/docs/middleware.py
@@ -8,6 +8,10 @@ class DocMiddleware(object):
     def process_request(self, request):
         """
         Load the document for this request, if required.
+
+        Example path: /app/docs/doc_id/
+
+        FIXME: What about /app/docs/doc.txt
         """
         parts = request.path_info.split('/')
         if len(parts) < 5 or parts[1] != 'app' or parts[2] != 'docs':
@@ -19,8 +23,10 @@ class DocMiddleware(object):
             # This request is going to create the document.
             # Create a dummy document that passes the permissions
             # check in AuthMiddleware.
-            key_name = '%s/%s' % (request.app.get_app_id(), doc_id)
             request.doc = Doc(key_name=key_name, writers=['public'])
+            # FIXME: This will stay public if the upload doesn't
+            # include writers.
+            # FIXME: Only app readers should be able to create docs.
             return
         # Check that the document exists.
         if request.doc is None:
