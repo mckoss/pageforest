6f0ba3e92577d6354f333dd48636a09ec1bc89e3
Johann C. Rocholl <johann@rocholl.net>
2010-05-13 12:00:18 -0700

    Merged ExceptionMiddleware into ResponseNotFoundMiddleware. SlashMiddleware now always adds a trailing slash.

--- appengine/apps/urls.py
+++ appengine/apps/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns(
     'apps.views',
     (r'^$', 'index'),
     (r'^(?P<app_id>%s)/$' % settings.APP_ID_REGEX, 'details'),
-    (r'^(?P<app_id>%s)/app.json$' % settings.APP_ID_REGEX, 'app_json'),
+    (r'^(?P<app_id>%s)/app.json/$' % settings.APP_ID_REGEX, 'app_json'),
 )
--- appengine/utils/middleware.py
+++ appengine/utils/middleware.py
@@ -4,6 +4,24 @@ from utils.shortcuts import render_to_response
 from django.shortcuts import redirect
 
 
+class ResponseNotFoundMiddleware(object):
+    """
+    Render HttpResponseNotFound (text/plain) into 404.html template.
+    This work-around is required as long as Django does not let us
+    raise Http404 from AppMiddleware or DocMiddleware.
+    """
+
+    def process_response(self, request, response):
+        if response.status_code == 404 and '<html' not in response.content:
+            request.exception = response.content
+            response = render_to_response(request, '404.html', {})
+            response.status_code = 404
+        return response
+
+    def process_exception(self, request, exception):
+        request.exception = exception
+
+
 class RequestMiddleware(object):
     """
     Save the request object in thread local storage to make it
@@ -38,38 +56,7 @@ class SlashMiddleware(object):
         if path in ('/stats', '/shell'):
             # Redirect to add trailing slash for app.yaml handlers.
             return redirect(path + '/')
-        if path.startswith('/docs/') and '/' in path[6:]:
-            # Don't mess with the key-value namespace.
-            return
-        filename = path.split('/')[-1]
-        if filename and '.' not in filename:
-            # URL ends with /word without file extension.
-            # Add a trailing slash (internal redirect).
-            request.path_info += '/'
-            request.META['PATH_INFO'] = request.path_info
-            request.path = request.META['SCRIPT_NAME'] + request.path_info
-
-
-class ExceptionMiddleware(object):
-    """
-    Stash information about any thrown exceptions into the request
-    object so it can be used by the 404, 500, and json templates.
-    """
-
-    def process_exception(self, request, exception):
-        request.exception = exception
-
-
-class ResponseNotFoundMiddleware(object):
-    """
-    Render HttpResponseNotFound (text/plain) into 404.html template.
-    This work-around is required as long as Django does not let us
-    raise Http404 from AppMiddleware or DocMiddleware.
-    """
-
-    def process_response(self, request, response):
-        if response.status_code == 404 and '<html' not in response.content:
-            request.exception = response.content
-            response = render_to_response(request, '404.html', {})
-            response.status_code = 404
-        return response
+        # Add a trailing slash (internal redirect).
+        request.path_info += '/'
+        request.META['PATH_INFO'] = request.path_info
+        request.path = request.META['SCRIPT_NAME'] + request.path_info
