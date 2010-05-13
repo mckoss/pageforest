5563c1487c78c24a881d265fc9c69fa9b2a79f64
Johann C. Rocholl <johann@rocholl.net>
2010-05-13 14:47:35 -0700

    Fixed and cleaned AuthMiddleware with Mike.

--- appengine/auth/middleware.py
+++ appengine/auth/middleware.py
@@ -4,10 +4,8 @@ from django.conf import settings
 from django.http import HttpResponseForbidden
 
 from auth.models import User, SignatureError
-from docs.middleware import DOC_REGEX
 
-SIGN_IN_REGEX = re.compile(r'^/sign-(up|in)/')
-APP_JSON_REGEX = re.compile(r'^/apps/(%s)/app.json$' % settings.APP_ID_REGEX)
+APP_JSON_REGEX = re.compile(r'^/apps/(%s)/app.json/$' % settings.APP_ID_REGEX)
 
 
 class AccessDenied(HttpResponseForbidden):
@@ -39,37 +37,41 @@ class AuthMiddleware(object):
     def process_request(self, request):
         """
         Attempt to extract the user from the session cookie into
-        request.user. If it fails for any reason, request.user is set
-        to None (fails silently). Then check read or write permissions
-        for the current app or document.
+        request.user. Then check read or write permissions for the
+        current app or document.
+
+        If the session cookie is invalid, request.user is set to None
+        and the error message is stored in request.session_key_error.
+        The permissions check may still succeed (e.g. for public
+        readable documents), but if it fails the stored error message
+        is returned with status 403 Forbidden.
         """
         request.user = None
-        if settings.SESSION_COOKIE_NAME not in request.COOKIES:
-            return
-        session_key = request.COOKIES[settings.SESSION_COOKIE_NAME]
-        try:
-            request.user = User.verify_session_key(session_key, request.app)
-        except SignatureError, error:
-            request.session_key_error = "Invalid %s cookie: %s" % (
-                settings.SESSION_COOKIE_NAME, unicode(error))
-            assert request.user is None
-
-        if SIGN_IN_REGEX.match(request.path_info) and request.method == 'POST':
-            # Allow anonymous users to POST to the sign-in pages.
-            return
+        if settings.SESSION_COOKIE_NAME in request.COOKIES:
+            try:
+                request.user = User.verify_session_key(
+                    request.COOKIES[settings.SESSION_COOKIE_NAME],
+                    request.app)
+            except SignatureError, error:
+                request.session_key_error = "Invalid %s cookie: %s" % (
+                    settings.SESSION_COOKIE_NAME, unicode(error))
+                assert request.user is None
 
         if APP_JSON_REGEX.match(request.path_info):
             # Permissions will be checked in the view function.
             return
 
-        if DOC_REGEX.match(request.path_info) and request.doc is None \
-                and request.method == 'PUT':
-            # Check app read permissions before creating a document.
-            return check_permissions(request, request.app, 'GET')
-
         if hasattr(request, 'doc'):
-            # Check permissions for current document or its blob store.
-            return check_permissions(request, request.doc)
+            if request.doc is None:
+                assert request.method == 'PUT'
+                # Check app read permissions before creating a document.
+                # Application readers = document creators.
+                if request.user is None:
+                    return AccessDenied(request)
+                return check_permissions(request, request.app, 'GET')
+            else:
+                # Check permissions for current document or its blob store.
+                return check_permissions(request, request.doc)
 
         # Check permissions for the current app.
         return check_permissions(request, request.app)
