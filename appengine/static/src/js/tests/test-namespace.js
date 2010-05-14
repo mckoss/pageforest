b3546d09543eed76c7c5986007909a5eb30ef25b
Mike Koss <mckoss@startpad.org>
2010-05-14 14:41:59 -0700

    methods function.

--- appengine/static/src/js/tests/test-namespace.js
+++ appengine/static/src/js/tests/test-namespace.js
@@ -1,9 +1,9 @@
 namespace.lookup('org.startpad.namespace.test').defineOnce(function(ns) {
-    util = namespace.util;
+    var util = namespace.util;
 
     ns.addTests = function(ts) {
         ts.addTest("copyArray", function(ut) {
-            var x = [1,2,3];
+            var x = [1, 2, 3];
             var y = util.copyArray(x);
             ut.assertEq(x, y);
             x[0] = 4;
@@ -13,16 +13,39 @@ namespace.lookup('org.startpad.namespace.test').defineOnce(function(ns) {
                 return util.copyArray(arguments);
             }
 
-            x = dummy(1,2,3,4,5);
-            ut.assertEq(x, [1,2,3,4,5]);
+            x = dummy(1, 2, 3, 4, 5);
+            ut.assertEq(x, [1, 2, 3, 4, 5]);
             ut.assertEq(typeof x, 'object');
         });
 
         ts.addTest("extendObject", function(ut) {
-            var x = {a:1, b:2};
-            var y = {c:3, d: 4};
+            var x = {a: 1, b: 2};
+            var y = {c: 3, d: 4};
             var z = util.extendObject(x, y);
-            ut.assertEq(z, {a:1, b:2, c:3, d:4});
+            ut.assertEq(z, {a: 1, b: 2, c: 3, d: 4});
+        });
+
+        ts.addTest("methods", function (ut) {
+            function Foo() {
+                this.x = 0;
+            }
+
+            Foo.methods({
+                a: function () {
+                    this.x++;
+                },
+
+                b: function () {
+                    this.x--;
+                }
+            });
+
+            var f = new Foo();
+            ut.assertEq(f.x, 0);
+            f.a();
+            ut.assertEq(f.x, 1);
+            f.b();
+            ut.assertEq(f.x, 0);
         });
 
         ts.addTest("lookup and define", function(ut) {
@@ -30,7 +53,7 @@ namespace.lookup('org.startpad.namespace.test').defineOnce(function(ns) {
             var self = namespace.lookup('org.startpad.namespace.test');
             ut.assertIdent(self, ns);
 
-            function nsDefinition (ns) {
+            function nsDefinition(ns) {
                 if (ns.x) {
                     ns.x++;
                 }
