a765cf34a6479c73b16e8da69b79e8fb064e2c29
Mike Koss <mckoss@startpad.org>
2010-05-14 06:29:31 -0700

    Run jstest quietly.

--- tools/jstest.py
+++ tools/jstest.py
@@ -2,21 +2,28 @@
 
 import os
 import sys
+from optparse import OptionParser, make_option
 
 import pftool
 
-DEBUG = False
-
-if DEBUG:
-    RHINO = 'java org.mozilla.javascript.tools.debugger.Main'
-else:
-    RHINO = 'java org.mozilla.javascript.tools.shell.Main'
+RHINO = 'java org.mozilla.javascript.tools.shell.Main'
+RHINO_DEBUG = 'java org.mozilla.javascript.tools.debugger.Main'
 
 if __name__ == '__main__':
-    command = RHINO + ' '
-    command += os.path.join(pftool.js_test_dir, 'test-runner-cl.js')
-    command += ' ' + ' '.join(sys.argv[1:])
-    print command
+    option_list = (
+        make_option('-q', '--quiet', action='store_true',
+                    help="just display error summary"),
+        make_option('-d', '--debug', action='store_true',
+                    help="run rhino debugger over tests"),
+        )
+    parser = OptionParser(option_list=option_list,
+        usage="%prog [options] module_names")
+    (options, args) = parser.parse_args()
+    command = options.debug and RHINO_DEBUG or RHINO
+    command += ' ' + os.path.join(pftool.js_test_dir, 'test-runner-cl.js')
+    if options.quiet:
+        command += ' -q'
+    command += ' ' + ' '.join(args)
     os.chdir(pftool.js_test_dir)
     code = os.system(command)
     sys.exit(code)
