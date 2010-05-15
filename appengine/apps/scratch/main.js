edc112b60e1c16cb062663401d4e37fb184f32ec
Mike Koss <mckoss@startpad.org>
2010-05-14 23:21:44 -0700

    Checkpoint - use client.js in scratch app - broken now.

--- appengine/apps/scratch/main.js
+++ appengine/apps/scratch/main.js
@@ -1,96 +1,55 @@
-namespace.lookup('com.pageforest.scratch').defineOnce(function (ns) {
-    var cookies = namespace.lookup('com.pageforest.cookies');
+namespace.lookup('com.pageforest.examples.scratch').defineOnce(function (ns) {
+    var client = namespace.lookup('com.pageforest.client');
 
-    ns.ready = function() {
-        $('#doc_id').focus();
-        ns.lastHash = location.hash;
-        setInterval(ns.poll, 1000);
-        if (location.hash) {
-            ns.load(location.hash.substr(1));
+    function App() {
+        this.client = new client.Client(this);
+    }
+
+    function init() {
+        ns.app = new App();
+    }
+
+    App.methods({
+        loaded: function (json) {
+            $('#title').val(json.title);
+            $('#blob').val(json.blob);
+            $('#docid').val(this.client.docid);
+            this.status("Loaded");
+        },
+
+        saved: function () {
+            this.status("Saved.");
+        },
+
+        error: function (errorMessage) {
+            this.status(errorMessage);
+        },
+
+        status: function (message) {
+            $('#results').prepend('<div>' + message +
+                                  '(' + this.client.state + ')' +
+                                  '</div>');
+        },
+
+        save: function () {
+            var json = {
+                'title': $('#title').val(),
+                'blob': $('#blob').val()
+                };
+            this.client.save(json, $('#docid'));
+        },
+
+        userChanged: function (username) {
+            $('#username').text(username);
+            var isSignedIn = username != 'anonymous';
+            $('#signin').attr('disabled', isSignedIn);
+            $('#signout').attr('disabled', !isSignedIn);
         }
-    };
+    });
 
-    ns.poll = function() {
-        if (ns.lastHash != location.hash) {
-            ns.lastHash = location.hash;
-            ns.load(location.hash.substr(1));
-        }
-        ns.checkUsername();
-    };
-
-    ns.checkUsername = function() {
-        var sessionkey = cookies.getCookie('sessionkey');
-        var username;
-
-        if (sessionkey !== undefined) {
-            username = sessionkey.split('/')[1];
-        }
-        else {
-            username = undefined;
-        }
-
-        $('#username').text(username || 'anonymous');
-        var isSignedIn = (username !== undefined);
-        $('#signin').attr('disabled', isSignedIn);
-        $('#signout').attr('disabled', !isSignedIn);
-    };
-
-    ns.signin = function() {
-        var dot = location.host.indexOf('.');
-        var www = "www" + location.host.substr(dot);
-        var url = "http://" + www + "/sign-in/scratch/";
-        window.open(url, '_blank');
-    };
-
-    ns.signout = function() {
-        document.cookie = 'sessionkey=expired; path=/; expires=' +
-            'Sat, 01 Jan 2000 00:00:00 GMT';
-    };
-
-    ns.save = function() {
-        var data = JSON.stringify({
-            title: $('#title').val(),
-            blob: $('#blob').val(),
-            readers: ['public']
-        });
-        var docId = $('#name').val();
-        $.ajax({
-            type: 'PUT',
-            url: '/docs/' + docId,
-            data: data,
-            complete: ns.showStatus,
-            success: function(data) {
-                location.hash = docId;
-            }
-        });
-    };
-
-    ns.load = function(docid) {
-        if (docid) {
-            $('#name').val(docid);
-        }
-        else {
-            docid = $('#name').val();
-        }
-        $.ajax({
-            url: '/docs/' + docid,
-            complete: ns.showStatus,
-            success: function(document) {
-                $('#title').val(document.title);
-                $('#blob').val(document.blob);
-            }
-        });
-    };
-
-    ns.showStatus = function(ajax, status) {
-        var now = new Date();
-        var time = now.getHours() + ':' +
-            (100 + now.getMinutes()).toString().substr(1) + ':' +
-            (100 + now.getSeconds()).toString().substr(1);
-        var message = '<div>' + time + ': ' + status;
-        message += ' (' + ajax.status + ')';
-        message += '</div>';
-        $('#results').prepend(message);
-    };
+    // Exported functions
+    ns.extend({
+        init: init
+    });
 
 });
