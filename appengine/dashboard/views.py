48294d3bdaf19c2ff871c934ee9e5400769ef63c
Johann C. Rocholl <johann@rocholl.net>
2010-05-15 14:56:30 -0700

    Sparkline charts over two weeks, with start and end dates.

--- appengine/dashboard/views.py
+++ appengine/dashboard/views.py
@@ -8,13 +8,8 @@ from utils.shortcuts import render_to_response
 
 from dashboard.models import StatsHour, StatsDay, StatsMonth
 
+SPARKLINE_HOURS = 14 * 24  # Two weeks.
 ENCODING = string.uppercase + string.lowercase + string.digits + '.-'
-
-
-def smooth(values):
-    for i in range(len(values)):
-        avg = int(sum([values[i - j] for j in range(10)]) / 10)
-        yield max(0, avg)
 
 
 def sparkline(hours, property_name):
@@ -24,30 +19,41 @@ def sparkline(hours, property_name):
             values.append(0)
         else:
             values.append(getattr(hour, property_name, None))
-    # values = list(smooth(values))
     maximum = max(values)
     divider = max(10, maximum)
     chars = []
     radix = len(ENCODING)
+    max_index = 0
     for index in range(len(values)):
         value = values[index] or 0
+        if value == maximum:
+            max_index = index
         value = value * 4095 / divider
         chars.append(ENCODING[value / radix] +
                      ENCODING[value % radix])
-    return ''.join(chars), values.index(maximum)
+    return ''.join(chars), maximum, max_index
 
 
 def chart(hours, property_name, color):
-    (chars, max_index) = sparkline(hours, property_name)
+    (chars, maximum, max_index) = sparkline(hours, property_name)
+    horizontal_offset = 0
+    if maximum > 8:
+        horizontal_offset = -10
     return '&'.join([
             'http://chart.apis.google.com/chart?cht=ls',
-            'chs=460x80',      # Size in pixels.
+            'chs=460x80',       # Size in pixels.
             'chf=bg,s,000000',  # Black background.
             'chco=' + color,    # Foreground.
             'chls=3',           # Line width in pixels.
-            'chm=N*f0*,%s,0,%d,16,,:10' % (color, max_index),
+            'chm=t%d,%s,0,%d,14,,h:%d:6' % (  # Marker label for maximum.
+                maximum, color, max_index, horizontal_offset),
             'chd=e:' + chars,
             ])
+
+
+def simple_date(date):
+    return ' '.join((date.strftime('%b'),
+                     date.strftime('%d').lstrip('0')))
 
 
 def dashboard(request):
@@ -56,17 +62,21 @@ def dashboard(request):
     """
     now = datetime.now()
     keys = []
-    for ago in range(24 * 7):
+    for ago in range(SPARKLINE_HOURS):
         start_time = now - timedelta(hours=ago)
         name = start_time.strftime('%Y%m%d%H')
         key = db.Key.from_path('StatsHour', name)
         keys.insert(0, key)
     hours = db.get(keys)
+    today = datetime.now()
+    start = today - timedelta(hours=SPARKLINE_HOURS)
     dictionary = {
         'users_chart': chart(hours, 'users', '00FF00'),
         'apps_chart': chart(hours, 'apps', '0080FF'),
         'docs_chart': chart(hours, 'docs', 'FFFF00'),
         'blobs_chart': chart(hours, 'blobs', 'FF0000'),
+        'start_date': simple_date(start),
+        'today_date': simple_date(today),
         }
     return render_to_response(request, 'dashboard/index.html', dictionary)
 
