789f05bf8340ee631542e5e6bd7071dfb4339e71
Mike Koss <mckoss@startpad.org>
2010-05-15 22:42:26 -0700

    format testing passing.

--- appengine/static/src/js/tests/test-base.js
+++ appengine/static/src/js/tests/test-base.js
@@ -1,84 +1,84 @@
 namespace.lookup('org.startpad.base.test').defineOnce(function (ns) {
-    util = namespace.util;
+    var util = namespace.util;
     var base = namespace.lookup('org.startpad.base');
 
-ns.addTests = function (ts) {
+    ns.addTests = function (ts) {
 
-ts.addTest("String Buffer", function(ut)
-{
-    var stb1 = new base.StBuf();
-    ut.assertEq(stb1.toString(), "");
+        ts.addTest("String Buffer", function(ut)
+        {
+            var stb1 = new base.StBuf();
+            ut.assertEq(stb1.toString(), "");
 
-    stb1.append("hello");
-    ut.assertEq(stb1.toString(), "hello");
+            stb1.append("hello");
+            ut.assertEq(stb1.toString(), "hello");
 
-    stb1.append(", mom");
-    ut.assertEq(stb1.toString(), "hello, mom");
+            stb1.append(", mom");
+            ut.assertEq(stb1.toString(), "hello, mom");
 
-    var stb2 = new base.StBuf();
-    stb2.append(stb1).append("-").append(stb1);
-    stb1.clear();
-    ut.assertEq(stb1.toString(), "");
-    ut.assertEq(stb2.toString(), "hello, mom-hello, mom");
+            var stb2 = new base.StBuf();
+            stb2.append(stb1).append("-").append(stb1);
+            stb1.clear();
+            ut.assertEq(stb1.toString(), "");
+            ut.assertEq(stb2.toString(), "hello, mom-hello, mom");
 
-    var stb3 = new base.StBuf();
-    stb3.append("this", ", that", ", the other");
-    ut.assertEq(stb3.toString(), "this, that, the other");
+            var stb3 = new base.StBuf();
+            stb3.append("this", ", that", ", the other");
+            ut.assertEq(stb3.toString(), "this, that, the other");
 
-    var stb4 = new base.StBuf("initial", " value");
-    ut.assertEq(stb4.toString(), "initial value");
-});
+            var stb4 = new base.StBuf("initial", " value");
+            ut.assertEq(stb4.toString(), "initial value");
+        });
 
-ts.addTest("Object Extension", function(ut)
-{
-    var obj1 = {a:1, b:"hello"};
-    util.extendObject(obj1, {c:3});
-    ut.assertEq(obj1, {a:1, b:"hello", c:3});
+        ts.addTest("Object Extension", function(ut)
+        {
+            var obj1 = {a: 1, b: "hello"};
+            util.extendObject(obj1, {c: 3});
+            ut.assertEq(obj1, {a: 1, b: "hello", c: 3});
 
-    base.extendIfMissing(obj1, {a:2, b:"mom", d:"new property"});
-    ut.assertEq(obj1, {a:1, b:"hello", c:3, d:"new property"});
+            base.extendIfMissing(obj1, {a: 2, b: "mom", d: "new property"});
+            ut.assertEq(obj1, {a: 1, b: "hello", c: 3, d: "new property"});
 
-    var obj2 = {};
-    util.extendObject(obj2, {a: 1}, {b: 2}, {a: 3});
-    ut.assertEq(obj2, {a: 3, b: 2});
+            var obj2 = {};
+            util.extendObject(obj2, {a: 1}, {b: 2}, {a: 3});
+            ut.assertEq(obj2, {a: 3, b: 2});
 
-    var a = [];
-    var b = [1,[2,3],4];
-    base.extendDeep(a, b);
-    ut.assertEq(a[0], 1);
-    ut.assertEq(a[1], [2,3]);
-    ut.assertEq(a[2], 4);
+            var a = [];
+            var b = [1, [2, 3], 4];
+            base.extendDeep(a, b);
+            ut.assertEq(a[0], 1);
+            ut.assertEq(a[1], [2, 3]);
+            ut.assertEq(a[2], 4);
 
-    var o1 = {};
-    var o2 = {a:1, b:{c:2}};
-    var o3 = {d:3};
-    base.extendDeep(o1, o2, o3);
-    ut.assertEq(o1, {a:1, b:{c:2}, d:3});
-    o1.b.c = 99;
-    ut.assertEq(o2, {a:1, b:{c:2}});
-});
+            var o1 = {};
+            var o2 = {a: 1, b: {c: 2}};
+            var o3 = {d: 3};
+            base.extendDeep(o1, o2, o3);
+            ut.assertEq(o1, {a: 1, b: {c: 2}, d: 3});
+            o1.b.c = 99;
+            ut.assertEq(o2, {a: 1, b: {c: 2}});
+        });
 
-ts.addTest("strip", function(ut)
-{
-    ut.assertEq(base.strip(" hello, mom "), "hello, mom");
-    ut.assertEq(base.strip(" leading"), "leading");
-    ut.assertEq(base.strip("trailing "), "trailing");
-    ut.assertEq(base.strip("inner space"), "inner space");
-    ut.assertEq(base.strip("     "), "");
-    ut.assertEq(base.strip("   \r\nWORD\r\n  "), "WORD");
-});
+        ts.addTest("strip", function(ut)
+        {
+            ut.assertEq(base.strip(" hello, mom "), "hello, mom");
+            ut.assertEq(base.strip(" leading"), "leading");
+            ut.assertEq(base.strip("trailing "), "trailing");
+            ut.assertEq(base.strip("inner space"), "inner space");
+            ut.assertEq(base.strip("     "), "");
+            ut.assertEq(base.strip("   \r\nWORD\r\n  "), "WORD");
+        });
 
-ts.addTest("Enum", function(ut)
-{
-    var e = new base.Enum("a", "b", "c");
-    ut.assertEq(e, {a:0, b:1, c:2});
-    e = new base.Enum(1, "a", "b", 5, "c");
-    ut.assertEq(e, {a:1, b:2, c:5});
-    e = new base.Enum();
-    ut.assertEq(e, {});
+        ts.addTest("Enum", function(ut)
+        {
+            var e = new base.Enum("a", "b", "c");
+            ut.assertEq(e, {a: 0, b: 1, c: 2});
+            e = new base.Enum(1, "a", "b", 5, "c");
+            ut.assertEq(e, {a: 1, b: 2, c: 5});
+            e = new base.Enum();
+            ut.assertEq(e, {});
 
-});
+        });
 
-}; // addTests
+    }; // addTests
 
 }); // org.startpad.base.test
---
+++ appengine/static/src/js/tests/test-format.js
@@ -0,0 +1,110 @@
+namespace.lookup('org.startpad.format.test').defineOnce(function (ns) {
+    var format = namespace.lookup('org.startpad.format');
+
+    ns.addTests = function(ts) {
+
+        ts.addTest("replaceKeys", function(ut)
+        {
+            ut.assertEq(format.replaceKeys("this is {wow} test", {wow: "foo"}),
+                        "this is foo test");
+            ut.assertEq(format.replaceKeys("{key} is replaced {key} twice",
+                                           {key: "yup"}),
+                        "yup is replaced yup twice");
+            ut.assertEq(format.replaceKeys("{key} and {key2}", {key: "mom"}),
+                        "mom and ");
+        });
+
+        ts.addTest("thousands", function (ut) {
+            var cases = [[100, '100'],
+                         [-100, '-100'],
+                         [1000, '1,000'],
+                         [-1000, '-1,000'],
+                         [1000000, '1,000,000']];
+            for (var i = 0; i < cases.length; i++) {
+                var acase = cases[i];
+                ut.assertEq(format.thousands(acase[0]), acase[1]);
+            }
+            ut.assertEq(format.thousands(1000, 2), '1,000.00');
+        });
+
+        ts.addTest("fixedDigits", function (ut) {
+            ut.assertEq(format.fixedDigits(1,2), "01");
+            ut.assertEq(format.fixedDigits(11, 10), "0000000011", "long numbers");
+            ut.assertEq(format.fixedDigits(123, 2), "23", "overflow");
+            ut.assertEq(format.fixedDigits(12.34, 2), "12", "fractions");
+            ut.assertEq(format.fixedDigits(-1, 2), "-01", "negative numbers");
+        });
+
+        ts.addTest("ISO 8601 Formatting", function(ut)
+        {
+            var aTest = [
+                [[1960, 8, 31], [0,0,0,0], "1960-08-31"],
+                [[1960, 8, 31], [7,8], "1960-08-31T07:08Z"],
+                [[1960, 8, 31], [7,8,9,333], "1960-08-31T07:08:09.333Z"]
+            ];
+
+            var dt = new Date();
+            ut.assertEq(dt.getTime(), dt.valueOf(), "Javascript assumption");
+            var sISO = format.iso(dt);
+            var sTZ = format.fixedDigits(-dt.getTimezoneOffset()/60, 2);
+            ut.assertEq(sISO.substring(sISO.length-3), sTZ, "Timezone");
+
+            // Fix dt as a UTC date/time
+            dt.__tz = 0;
+            sISO = format.iso(dt);
+            ut.assertEq(sISO.substring(sISO.length-1), "Z", "Timezone - fixed at UTC: " + sISO);
+            for (var i = 0; i < aTest.length; i++)
+                {
+                ut.Trace(i);
+                var aDate = aTest[i][0];
+                aDate[1]--;
+                var aTime = aTest[i][1];
+                var sISO = aTest[i][2];
+                dt.setUTCFullYear.apply(dt, aDate);
+                dt.setUTCHours.apply(dt, aTime);
+                ut.assertEq(format.iso(dt), sISO);
+                }
+
+            dt.setUTCFullYear(1995, 0, 15);
+            dt.setUTCHours(0,0,0,0);
+            ut.assertEq(format.iso(dt, true), "1995-01-15T00:00Z");
+        }).enable(false);
+
+        ts.addTest("ISO 8601 Parsing", function(ut)
+        {
+            var dt = new Date();
+
+            var aTest = [
+            // ISO, UTC: [Y,M,D], [h,m,s,ms], tz
+            ["1984-01-01", [1984, 1, 1], [0,0,0,0], 0],
+            ["", undefined],
+            ["1984", undefined],
+            ["1984-01", undefined],
+            ["1984-01-01T01:02:03.456-07", [1984,1,1], [8,2,3,456], -7],
+            ["1984-01-01T01:02:03.456Z", [1984,1,1], [1,2,3,456], 0],
+            ["19840101T010203.456Z", [1984,1,1], [1,2,3,456], 0],
+            ["19840101 010203.456Z", undefined],
+            ["1984-01-01T01:02:03X456-07", undefined]
+            ];
+
+            for (var i = 0; i < aTest.length; i++)
+                {
+                ut.Trace(i);
+                var aDate = aTest[i][1];
+                if (!aDate)
+                    {
+                    ut.assertEq(format.parseISO(aTest[i][0]), undefined);
+                    continue;
+                    }
+                aDate[1]--;
+                var aTime = aTest[i][2];
+                dt.setUTCFullYear.apply(dt, aDate);
+                dt.setUTCHours.apply(dt, aTime);
+                dt.__tz = aTest[i][3];
+                ut.assertEq(format.parseISO(aTest[i][0]), dt);
+                }
+        }).enable(false);
+
+    }; // addTests
+
+});  // org.startpad.format.test
