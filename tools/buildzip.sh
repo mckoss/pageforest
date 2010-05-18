ba7499b914f93d2ce10d08df50abbb518dee7a63
Johann C. Rocholl <johann@rocholl.net>
2010-05-17 21:17:11 -0700

    Moved appengine/apps/scratch to examples/scratch because we're going to have a clone button for each demo application.

--- tools/buildzip.sh
+++ tools/buildzip.sh
@@ -3,7 +3,7 @@ DIR=scratch
 VERSION=`grep ^LIB_VERSION appengine/settings.py | cut -d\' -f2`
 ZIPFILE=scratch-$VERSION.zip
 echo building: $ZIPFILE
-cp -r appengine/apps/scratch $DIR || exit
+cp -r examples/scratch $DIR || exit
 cp tools/pf.py $DIR
 rm -f $ZIPFILE
 zip -r $ZIPFILE $DIR
