79fad0e334751e44f3338b16850758df9d73b039
Johann C. Rocholl <johann@rocholl.net>
2010-05-18 11:07:03 -0700

    Added timestamp to zip file name, and show title for copy/paste during upload.

--- tools/buildzip.sh
+++ tools/buildzip.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 DIR=scratch
 VERSION=`grep ^LIB_VERSION appengine/settings.py | cut -d\' -f2`
-ZIPFILE=scratch-$VERSION.zip
+TIMESTAMP=`date +%Y%m%d-%H%M%S`
+ZIPFILE=scratch-$VERSION-$TIMESTAMP.zip
 echo building: $ZIPFILE
 cp -r examples/scratch $DIR || exit
 rm -f $DIR/.passwd
@@ -9,3 +10,4 @@ cp tools/pf.py $DIR
 rm -f $ZIPFILE
 zip -r $ZIPFILE $DIR
 rm -rf $DIR
+grep title examples/scratch/app.json | cut -d\" -f4
