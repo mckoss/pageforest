7401d2e06098c828f73b0f281b4bf84f6b65fbb5
Johann C. Rocholl <johann@rocholl.net>
2010-05-17 21:32:31 -0700

    Don't include .passwd file in scratch-0.5.2.zip.

--- tools/buildzip.sh
+++ tools/buildzip.sh
@@ -4,6 +4,7 @@ VERSION=`grep ^LIB_VERSION appengine/settings.py | cut -d\' -f2`
 ZIPFILE=scratch-$VERSION.zip
 echo building: $ZIPFILE
 cp -r examples/scratch $DIR || exit
+rm -f $DIR/.passwd
 cp tools/pf.py $DIR
 rm -f $ZIPFILE
 zip -r $ZIPFILE $DIR
