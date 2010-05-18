37afe38d8a9d0644e05fbf3adbde2452b94253e7
Johann C. Rocholl <johann@rocholl.net>
2010-05-17 21:32:52 -0700

    Bumped LIB_VERSION to 0.5.2.

--- appengine/lib/0/js/utils.js
+++ appengine/lib/0/js/utils.js
@@ -505,6 +505,119 @@ namespace.lookup('org.startpad.cookies').define(function(ns) {
     }}); // ns
 
 }); // org.startpad.cookies
+/* Begin file: format.js */
+namespace.lookup('org.startpad.format').defineOnce(function(ns) {
+    var base = namespace.lookup('org.startpad.base');
+
+    // Thousands separator
+    var comma = ',';
+
+    // Return an integer as a string using a fixed number of digits,
+    // (require a sign if fSign).
+    function fixedDigits(value, digits, fSign) {
+        var s = "";
+        var fNeg = (value < 0);
+        if (digits == undefined) {
+            digits = 0;
+        }
+        if (fNeg) {
+            value = -value;
+        }
+        value = Math.floor(value);
+
+        for (; digits > 0; digits--) {
+            s = (value % 10) + s;
+            value = Math.floor(value / 10);
+        }
+
+        if (fSign || fNeg) {
+            s = (fNeg ? "-" : "+") + s;
+        }
+
+        return s;
+    }
+
+    // Return integer as string with thousand separators with optional
+    // decimal digits.
+    function thousands(value, digits) {
+        var integerPart = Math.floor(value);
+        var s = value.toString();
+        var sLast = "";
+        while (s != sLast) {
+            sLast = s;
+            s = s.replace(/(\d+)(\d{3})/, "$1" + comma + "$2");
+        }
+
+        var fractionString = "";
+        if (digits && digits > 0) {
+            var fraction = value - integerPart;
+            fraction = Math.floor(fraction * Math.pow(10, digits));
+            fractionString = "." + fixedDigits(fraction, digits);
+        }
+        return s + fractionString;
+    }
+
+    // Converts to lowercase, removes non-alpha chars and converts
+    // spaces to hyphens
+    function slugify(s) {
+        s = base.strip(s).toLowerCase();
+        s = s.replace(/[^a-zA-Z0-9]/g, '-').
+              replace(/[\-]+/g, '-').
+              replace(/(^-+)|(-+$)/g, '');
+        return s;
+    }
+
+    function escapeHTML(s) {
+        s = s.toString();
+        s = s.replace(/&/g, '&amp;');
+        s = s.replace(/</g, '&lt;');
+        s = s.replace(/>/g, '&gt;');
+        s = s.replace(/\"/g, '&quot;');
+        s = s.replace(/'/g, '&#39;');
+        return s;
+    }
+
+    // Replace all instances of pattern, with replacement in string.
+    function replaceString(string, pattern, replacement) {
+        var output = "";
+        if (replacement == undefined) {
+            replacement = "";
+        }
+        else {
+            replacement = replacement.toString();
+        }
+        var ich = 0;
+        var ichFind = string.indexOf(pattern, 0);
+        while (ichFind >= 0) {
+            output += string.substring(ich, ichFind) + replacement;
+            ich = ichFind + pattern.length;
+            ichFind = string.indexOf(pattern, ich);
+        }
+        output += string.substring(ich);
+        return output;
+    }
+
+    // Replace keys in dictionary of for {key} in the text string.
+    function replaceKeys(st, keys) {
+        for (var key in keys) {
+            if (keys.hasOwnProperty(key)) {
+                st = replaceString(st, "{" + key + "}", keys[key]);
+            }
+        }
+        // remove unused keys
+        st = st.replace(/\{[^\{\}]*\}/g, "");
+        return st;
+    }
+
+    ns.extend({
+        fixedDigits: fixedDigits,
+        thousands: thousands,
+        slugify: slugify,
+        escapeHTML: escapeHTML,
+        replaceKeys: replaceKeys,
+        replaceString: replaceString
+    });
+}); // org.startpad.format
 /* Begin file: client.js */
 /*
   client.js - Pageforest client api for sign in, save, load, and url
@@ -751,116 +864,3 @@ namespace.lookup('com.pageforest.client').defineOnce(function (ns) {
     });
 
 });
-/* Begin file: format.js */
-namespace.lookup('org.startpad.format').defineOnce(function(ns) {
-    var base = namespace.lookup('org.startpad.base');
-
-    // Thousands separator
-    var comma = ',';
-
-    // Return an integer as a string using a fixed number of digits,
-    // (require a sign if fSign).
-    function fixedDigits(value, digits, fSign) {
-        var s = "";
-        var fNeg = (value < 0);
-        if (digits == undefined) {
-            digits = 0;
-        }
-        if (fNeg) {
-            value = -value;
-        }
-        value = Math.floor(value);
-
-        for (; digits > 0; digits--) {
-            s = (value % 10) + s;
-            value = Math.floor(value / 10);
-        }
-
-        if (fSign || fNeg) {
-            s = (fNeg ? "-" : "+") + s;
-        }
-
-        return s;
-    }
-
-    // Return integer as string with thousand separators with optional
-    // decimal digits.
-    function thousands(value, digits) {
-        var integerPart = Math.floor(value);
-        var s = value.toString();
-        var sLast = "";
-        while (s != sLast) {
-            sLast = s;
-            s = s.replace(/(\d+)(\d{3})/, "$1" + comma + "$2");
-        }
-
-        var fractionString = "";
-        if (digits && digits > 0) {
-            var fraction = value - integerPart;
-            fraction = Math.floor(fraction * Math.pow(10, digits));
-            fractionString = "." + fixedDigits(fraction, digits);
-        }
-        return s + fractionString;
-    }
-
-    // Converts to lowercase, removes non-alpha chars and converts
-    // spaces to hyphens
-    function slugify(s) {
-        s = base.strip(s).toLowerCase();
-        s = s.replace(/[^a-zA-Z0-9]/g, '-').
-              replace(/[\-]+/g, '-').
-              replace(/(^-+)|(-+$)/g, '');
-        return s;
-    }
-
-    function escapeHTML(s) {
-        s = s.toString();
-        s = s.replace(/&/g, '&amp;');
-        s = s.replace(/</g, '&lt;');
-        s = s.replace(/>/g, '&gt;');
-        s = s.replace(/\"/g, '&quot;');
-        s = s.replace(/'/g, '&#39;');
-        return s;
-    }
-
-    // Replace all instances of pattern, with replacement in string.
-    function replaceString(string, pattern, replacement) {
-        var output = "";
-        if (replacement == undefined) {
-            replacement = "";
-        }
-        else {
-            replacement = replacement.toString();
-        }
-        var ich = 0;
-        var ichFind = string.indexOf(pattern, 0);
-        while (ichFind >= 0) {
-            output += string.substring(ich, ichFind) + replacement;
-            ich = ichFind + pattern.length;
-            ichFind = string.indexOf(pattern, ich);
-        }
-        output += string.substring(ich);
-        return output;
-    }
-
-    // Replace keys in dictionary of for {key} in the text string.
-    function replaceKeys(st, keys) {
-        for (var key in keys) {
-            if (keys.hasOwnProperty(key)) {
-                st = replaceString(st, "{" + key + "}", keys[key]);
-            }
-        }
-        // remove unused keys
-        st = st.replace(/\{[^\{\}]*\}/g, "");
-        return st;
-    }
-
-    ns.extend({
-        fixedDigits: fixedDigits,
-        thousands: thousands,
-        slugify: slugify,
-        escapeHTML: escapeHTML,
-        replaceKeys: replaceKeys,
-        replaceString: replaceString
-    });
-}); // org.startpad.format
