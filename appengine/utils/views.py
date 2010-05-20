f357d6e53df8e42ad53f8c5be50c35ceb6907322
Johann C. Rocholl <johann@rocholl.net>
2010-05-20 12:05:34 -0700

    Added *args and **kwargs to reserved_url to fix error 500 on app_id.pageforest.com/docs.

--- appengine/utils/views.py
+++ appengine/utils/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.http import HttpResponseNotFound
 
 
-def reserved_url(request):
+def reserved_url(request, *args, **kwargs):
     """Return 404 Not Found with explanation."""
     return HttpResponseNotFound(
         "This url is reserved for internal use of %s." %
