0cddabb36f8aa7b24eeb57718ab9c4e72cfbc30e
Johann C. Rocholl <johann@rocholl.net>
2010-05-21 17:28:43 -0700

    Let admin list recent backups and download zipfiles.

--- appengine/backups/urls.py
+++ appengine/backups/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import patterns, handler404, handler500
 
-urlpatterns = patterns('backups.views',
+urlpatterns = patterns(
+    'backups.views',
+    (r'^$', 'index'),
+    (r'^(\w+-\d+-\d+.zip)/$', 'download'),
     (r'^users/cron/$', 'backup_users'),
     (r'^apps/cron/$', 'backup_apps'),
     (r'^docs/cron/$', 'backup_docs'),
