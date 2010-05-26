c9580d6c240dcdd900dbb1cf6e4a9bc1ad5a3874
Johann C. Rocholl <johann@rocholl.net>
2010-05-26 09:22:31 -0700

    Cleaned up.

--- appengine/utils/cookies.py
+++ appengine/utils/cookies.py
@@ -1,11 +1,9 @@
-from django.conf import settings
-
 import Cookie
 
 
-class Morsel(Cookie.Morsel):
+class NewMorsel(Cookie.Morsel):
     """
-    Provide support for HttpOnly cookies before Python 2.6.
+    Add support for HttpOnly cookies before Python 2.6.
     """
 
     def __setitem__(self, K, V):
@@ -13,30 +11,22 @@ class Morsel(Cookie.Morsel):
         if K == "httponly":
             dict.__setitem__(self, K, V)
         else:
-            super(Morsel, self).__setitem__(K, V)
+            super(NewMorsel, self).__setitem__(K, V)
 
     def OutputString(self, attrs=None):
-        output = super(Morsel, self).OutputString(attrs)
+        output = super(NewMorsel, self).OutputString(attrs)
         if self.get("httponly", "") and 'httponly' not in output.lower():
-                output += "; HttpOnly"
+            output += "; HttpOnly"
         return output
 
-    def debug(self):
-        print 'key:', self.key
-        print 'value:', self.value
-        print 'coded:', self.coded_value
-        for key in self:
-            if self[key]:
-                print key + ':', self[key]
 
-
-class SimpleCookie(Cookie.SimpleCookie):
+class NewCookie(Cookie.SimpleCookie):
     """
-    Cookie dictionary using our own Morsel subclass above.
+    Cookie dictionary using the NewMorsel subclass above.
     """
 
     def __set(self, key, real_value, coded_value):
-        M = self.get(key, Morsel())
+        M = self.get(key, NewMorsel())
         M.set(key, real_value, coded_value)
         dict.__setitem__(self, key, M)
 
@@ -46,14 +36,16 @@ class SimpleCookie(Cookie.SimpleCookie):
 
 
 class HttpOnlyMiddleware:
+    """
+    Replace the response cookie to add support for HttpOnly.
+    """
 
     def process_response(self, request, response):
-        if settings.SESSION_COOKIE_NAME in response.cookies:
-            original = response.cookies
-            response.cookies = SimpleCookie()
-            for name in original:
-                response.cookies[name] = original[name].value
-                response.cookies[name].update(original[name])
-                if not name.endswith('_exists'):
-                    response.cookies[name]['httponly'] = "true"
+        original = response.cookies
+        response.cookies = NewCookie()
+        for name in original:
+            response.cookies[name] = original[name].value
+            response.cookies[name].update(original[name])
+            if not name.endswith('_exists'):
+                response.cookies[name]['httponly'] = "true"
         return response
