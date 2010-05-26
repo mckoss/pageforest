becd8784402cdcc40535b0508121cefede88217f
Mike Koss <mckoss@startpad.org>
2010-05-26 00:47:32 -0700

    Sign-out does not work without a deep sessionkey delete.

--- appengine/static/2/js/pageforest.js
+++ appengine/static/2/js/pageforest.js
@@ -651,7 +651,6 @@ namespace.lookup('com.pageforest.auth.sign-in-form').define(function(ns) {
                 return;
             }
 
-
             // Check (once) if we're also currently logged in @ appId
             // without having to sign-in again.
             // REVIEW: Isn't this insecure?
