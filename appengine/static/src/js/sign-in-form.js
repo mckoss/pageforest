4e0cca80922c3e3d53e0a2210478d12e018cf865
Johann C. Rocholl <johann@rocholl.net>
2010-05-28 09:55:31 -0700

    Renamed documentReady to onReady, moved ns.extend to the bottom of the file to remove one level of indentation and allow normal syntax for function definitions.

--- appengine/static/src/js/sign-in-form.js
+++ appengine/static/src/js/sign-in-form.js
@@ -10,104 +10,101 @@ namespace.lookup('com.pageforest.auth.sign-in-form').define(function(ns) {
     */
     var cookies = namespace.lookup('org.startpad.cookies');
 
-    ns.extend({
-        // Check if user is already logged in.
-        documentReady: function(username, appId) {
-            ns.appId = appId;
-            ns.appAuthURL = 'http://' + ns.getAppDomain(appId) + '/auth/';
+    // www.pageforest.com -> app.pageforest.com
+    // pageforest.com -> app.pageforest.com
+    function getAppDomain(appId) {
+        var parts = window.location.host.split('.');
+        if (parts[0] == 'www') {
+            parts[0] = appId;
+        } else {
+            parts.splice(0, 0, appId);
+        }
+        return parts.join('.');
+    }
 
-            // Check for a (session) cookie with the application
-            // session key. We clear it once used so it doesn't get
-            // retransmitted. This could be used to either sign-in OR
-            // sign-out of the application.
-            var sessionName = appId + "-sessionkey";
-            var appSession = cookies.getCookies()[sessionName];
-            console.log("appSession: ", appSession);
-            if (appSession != undefined) {
-                cookies.setCookie(sessionName, 'expired', -1);
-                ns.transferSession(appSession);
+    // Use JSONP to read the username from the cross-site application.
+    function getJSONP(url, fn) {
+        $.ajax({
+            type: "GET",
+            url: url,
+            dataType: "jsonp",
+            success: fn,
+            error: function() {
+                fn({status: 500});
             }
+        });
+    }
 
-            // Nothing to do until the user signs in - page will reload
-            // on form post.
-            if (!username) {
+    // Display success, and close window in 5 seconds.
+    function closeForm() {
+        if (ns.appId) {
+            $(".have_app").show();
+        }
+        $(".want_app").hide();
+        setTimeout(window.close, 2000);
+    }
+
+    // Send a valid appId sessionKey to the app domain
+    // to get it installed on a cookie.
+    function transferSession(sessionKey) {
+        var url = ns.appAuthURL + "set-session/" + sessionKey;
+        getJSONP(url, function(message) {
+            if (typeof(message) != 'string') {
                 return;
             }
-
-            // Just logging in to pageforest - done.
-            if (!appId) {
-                ns.closeForm();
-                return;
+            // Close the window if this was used to
+            // sign in to the app.
+            if (sessionKey) {
+                closeForm();
             }
+        });
+    }
 
-            // Check (once) if we're also currently logged in @ appId
-            // without having to sign-in again.
-            // REVIEW: Isn't this insecure?
-            var url = ns.appAuthURL + "username/";
-            ns.getJSONP(url, function(username) {
-                // We're already logged in!
-                if (typeof(username) == 'string') {
-                    ns.closeForm();
-                    return;
-                }
-            });
-        },
+    // Check if user is already logged in.
+    function onReady(username, appId) {
+        ns.appId = appId;
+        ns.appAuthURL = 'http://' + getAppDomain(appId) + '/auth/';
 
-        transferSession: function(sessionKey) {
-            // Send a valid appId sessionKey to the app domain
-            // to get it installed on a cookie.
-            var url = ns.appAuthURL + "set-session/" + sessionKey;
-            ns.getJSONP(url, function(message) {
-                if (typeof(message) != 'string') {
-                    return;
-                }
-                // Close the window if this was used to
-                // sign in to the app.
-                if (sessionKey) {
-                    ns.closeForm();
-                }
-            });
-        },
-
-        // www.pageforest.com -> app.pageforest.com
-        // pageforest.com -> app.pageforest.com
-        getAppDomain: function(appId) {
-            var parts = window.location.host.split('.');
-            if (parts[0] == 'www') {
-                parts[0] = appId;
-            }
-            else {
-                parts.splice(0, 0, appId);
-            }
-            return parts.join('.');
-        },
-
-        // Use JSONP to read the username from the cross-site application.
-        getJSONP: function(url, fn) {
-            $.ajax({
-                type: "GET",
-                url: url,
-                dataType: "jsonp",
-                success: fn,
-                error: function() {
-                    fn({status: 500});
-                }
-            });
-        },
-
-        // Display success, and close window in 5 seconds.
-        closeForm: function() {
-            function closeFinal() {
-                // Close the window if we were opened by a cross-site script
-                window.close();
-            }
-            if (ns.appId) {
-                $(".have_app").show();
-            }
-            $(".want_app").hide();
-            setTimeout(closeFinal, 2000);
+        // Check for a (session) cookie with the application
+        // session key. We clear it once used so it doesn't get
+        // retransmitted. This could be used to either sign-in OR
+        // sign-out of the application.
+        var sessionName = appId + "-sessionkey";
+        var appSession = cookies.getCookies()[sessionName];
+        console.log("appSession: ", appSession);
+        if (appSession != undefined) {
+            cookies.setCookie(sessionName, 'expired', -1);
+            transferSession(appSession);
         }
 
-    }); // ns.extend
+        // Nothing to do until the user signs in - page will reload
+        // on form post.
+        if (!username) {
+            return;
+        }
+
+        // Just logging in to pageforest - done.
+        if (!appId) {
+            closeForm();
+            return;
+        }
+
+        // Check (once) if we're also currently logged in @ appId
+        // without having to sign-in again.
+        // REVIEW: Isn't this insecure?
+        var url = ns.appAuthURL + "username/";
+        getJSONP(url, function(username) {
+            // We're already logged in!
+            if (typeof(username) == 'string') {
+                closeForm();
+                return;
+            }
+        });
+    }
+
+    ns.extend({
+        transferSession: transferSession,
+        onReady: onReady
+    });
 
 }); // com.pageforest.sign-in-form
