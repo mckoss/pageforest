cf1dfac3c6412ef8499b054c45721a8322b988f2
Mike Koss <mckoss@startpad.org>
2010-05-29 17:18:20 -0700

    Passing speed unit test - 400ms.

--- examples/mandelbrot/mandelbrot.js
+++ examples/mandelbrot/mandelbrot.js
@@ -1,4 +1,5 @@
 namespace.lookup('com.pageforest.mandelbrot').defineOnce(function (ns) {
+    // http://en.wikipedia.org/wiki/Mandelbrot_set
 
     function Mandelbrot() {
         this.maxIterations = 1000;
@@ -17,9 +18,20 @@ namespace.lookup('com.pageforest.mandelbrot').defineOnce(function (ns) {
             var y = y0;
             var xT;
 
+            var x2 = x * x;
+            var y2 = y * y;
+
+            // Filter out points in the main cardiod
+            if (-0.75 < x && x < 0.38 && y < 0.66) {
+                var q = (x - 0.25) * (x - 0.25) + y2;
+                if (q * (q + x - 0.25) < 0.25 * y2) {
+                    return undefined;
+                }
+            }
+
             // Filter out points in bulb of radius 1/4 around (-1,0)
             if (-1.25 < x && x < -0.75 && y < 0.25) {
-                var d = (x + 1) * (x + 1) + y * y;
+                var d = (x + 1) * (x + 1) + y2;
                 if (d < 1 / 16) {
                     return undefined;
                 }
