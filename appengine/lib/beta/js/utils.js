9f55a4949e855eb7595a02a08cd0edd333ad20ad
Johann C. Rocholl <johann@rocholl.net>
2010-06-09 00:25:14 -0700

    Build.

--- appengine/lib/beta/js/utils.js
+++ appengine/lib/beta/js/utils.js
@@ -775,7 +775,9 @@ namespace.lookup('com.pageforest.client').defineOnce(function (ns) {
             }
         }
 
-        this.setCleanDoc(undefined, true);
+        if (this.app.getDoc) {
+            this.setCleanDoc(undefined, true);
+        }
 
         // REVIEW: When we support multiple clients per page, we can
         // combine all the poll functions into a shared one.
@@ -803,6 +805,10 @@ namespace.lookup('com.pageforest.client').defineOnce(function (ns) {
 
         // Load a document as the default document for this running application.
         load: function (docid) {
+            if (this.app.setDoc == undefined) {
+                return;
+            }
+
             // Your data is on notice.
             if (this.isDirty()) {
                 if (!this.confirmDiscard()) {
