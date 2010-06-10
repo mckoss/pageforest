f0d6d386f77bf9bf8ebc4a2838fd773de1552967
Johann C. Rocholl <johann@rocholl.net>
2010-06-09 18:06:08 -0700

    Introduced separate Chunk model to store Blob data if it's larger than 600 bytes. This is handled transparently by overriding __init__, __setattr__ and __getattribute__ in the Blob class, so all other code can continue to use blob.value directly.

--- appengine/blobs/models.py
+++ appengine/blobs/models.py
@@ -9,6 +9,22 @@ from utils.mixins import Timestamped, Migratable, Taggable, Cacheable
 from utils.mime import guess_mimetype
 from utils.json import is_valid_json
 
+# Don't attempt json.loads if we already know it's not valid.
+CERTAINLY_NOT_JSON = ['text/html', 'text/css', 'application/pdf']
+
+# Small data is stored directly in Blob.value, without separate Chunk.
+# This is really small so that we can LIST up to 1000 entities without
+# exceeding 1 MB for the datastore result, and because multiple copies
+# use redundant storage instead of pointing to the same Chunk.
+MAX_INTERNAL_SIZE = 600  # bytes
+
+
+class Chunk(Cacheable):
+    """
+    The key name is the SHA-1 hash of the content.
+    """
+    value = db.BlobProperty()
+
 
 class Blob(Timestamped, Migratable, Taggable, Cacheable):
     """
@@ -25,6 +41,56 @@ class Blob(Timestamped, Migratable, Taggable, Cacheable):
     sha1 = db.StringProperty(indexed=False)
     valid_json = db.BooleanProperty(indexed=False)
     directory = db.StringProperty()
+
+    # Schema version for Migratable mixin:
+    current_schema = 2
+
+    def __init__(self, *args, **kwargs):
+        super(Blob, self).__init__(*args, **kwargs)
+        if 'key_name' in kwargs:
+            key_parts = kwargs['key_name'].rstrip('/').split('/')
+            self.directory = '/'.join(key_parts[:-1]) + '/'
+        if 'value' in kwargs:
+            self.value = kwargs['value']
+
+    def __getattribute__(self, name):
+        """
+        Get data from self.value or a separate Chunk.
+        """
+        # print '__getattribute__', name
+        result = db.Model.__getattribute__(self, name)
+        if name == 'value' and result is None:
+            chunk = Chunk.get_by_key_name(self.sha1)
+            if chunk is None:
+                return None
+            return chunk.value
+        return result
+
+    def __setattr__(self, name, value):
+        """
+        When setting blob.value, also update size, sha1 and
+        valid_json, and put large value in a separate Chunk.
+        """
+        # print '__setattr__', name, value
+        if name != 'value':
+            db.Model.__setattr__(self, name, value)
+            return
+        self.size = len(value)
+        self.sha1 = sha1(value).hexdigest()
+        # Attempt to parse JSON, unless the file extension indicates a
+        # well-known MIME type that doesn't allow JSON data.
+        mimetype = guess_mimetype(self.key().name().rstrip('/'))
+        if mimetype in CERTAINLY_NOT_JSON or mimetype.startswith('image/'):
+            self.valid_json = False
+        else:
+            self.valid_json = is_valid_json(value)
+        # Store value in a separate Chunk if it's large.
+        if self.size <= MAX_INTERNAL_SIZE:
+            super(Blob, self).__setattr__(name, value)
+        else:
+            super(Blob, self).__setattr__(name, None)
+            if not Chunk.exists(self.sha1):
+                Chunk(key_name=self.sha1, value=value).put()
 
     def get_absolute_url(self):
         """
@@ -52,28 +118,19 @@ class Blob(Timestamped, Migratable, Taggable, Cacheable):
         Mixin properties like schema and modified are not cloned,
         they will be set automatically for the new entity.
         """
-        return Blob(
+        result = Blob(
             key_name=key_name,
-            value=self.value,
+            size=self.size,
             sha1=self.sha1,
-            valid_json=self.valid_json,
-            directory=self.directory)
+            valid_json=self.valid_json)
+        # Copy the internal value, but don't load Chunk from datastore.
+        result._value = self._value
+        return result
 
-    def put(self):
+    def migrate(self, next_schema):
         """
-        Update size, sha1, valid_json and directory properties
-        automatically before each datastore put.
+        Migrate from one model schema to the next.
         """
-        key_parts = self.key().name().rstrip('/').split('/')
-        self.directory = '/'.join(key_parts[:-1]) + '/'
-        self.size = len(self.value)
-        self.sha1 = sha1(self.value).hexdigest()
-        # Attempt to parse JSON unless the file extension indicates a
-        # well-known MIME type that doesn't allow JSON data.
-        mimetype = guess_mimetype(key_parts[-1])
-        if mimetype.startswith('image/') or mimetype in [
-            'text/html', 'text/css', 'application/pdf']:
-            self.valid_json = False
-        else:
-            self.valid_json = is_valid_json(self.value)
-        super(Blob, self).put()
+        if next_schema == 2:
+            assert self.schema == 1
+            self.__setattr__('value', self.value)
