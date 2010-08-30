f239b4e3f8b07250db8e511d6eef9471b39aed1a
Mike Koss <mckoss@startpad.org>
2010-08-30 10:42:20 -0700

    Fix jslint errors.

--- appengine/static/src/js/tests/test-base.js
+++ appengine/static/src/js/tests/test-base.js
@@ -188,6 +188,10 @@ namespace.lookup('org.startpad.base.test').defineOnce(function (ns) {
                 return arguments;
             }
 
+            // Avoid jslint error about using these constructors.
+            var S = String;
+            var N = Number;
+
             var equalTests = [
                 [undefined, undefined],
                 [null, null],
@@ -207,8 +211,8 @@ namespace.lookup('org.startpad.base.test').defineOnce(function (ns) {
                 [[1, [2, 3], 4], [1, [2, 3], 4]],
                 [{a: [1, 2, {c: 3}], b: [4, 5]},
                  {b: [4, 5], a: [1, 2, {c: 3, d: undefined}]}],
-                ["abc", new String("abc")],
-                [1.23, new Number(1.23)]
+                ["abc", new S("abc")],
+                [1.23, new N(1.23)]
             ];
 
             var d1 = new Date(2010, 7, 28);
@@ -238,16 +242,20 @@ namespace.lookup('org.startpad.base.test').defineOnce(function (ns) {
                  {b: [4, 5], a: [1, 2, {c: 6}]}]
             ];
 
+            var test;
+
             for (i = 0; i < equalTests.length; i++) {
                 ut.trace("equal #" + i);
-                var test = equalTests[i];
-                ut.assert(base.isEqual(test[0], test[1]), JSON.stringify(test[0]));
+                test = equalTests[i];
+                ut.assert(base.isEqual(test[0], test[1]),
+                          JSON.stringify(test[0]));
             }
 
             for (i = 0; i < unequalTests.length; i++) {
                 ut.trace("unequal #" + i);
-                var test = unequalTests[i];
-                ut.assert(!base.isEqual(test[0], test[1]), JSON.stringify(test[0]));
+                test = unequalTests[i];
+                ut.assert(!base.isEqual(test[0], test[1]),
+                          JSON.stringify(test[0]));
             }
 
             function A(a, b) {
@@ -260,7 +268,8 @@ namespace.lookup('org.startpad.base.test').defineOnce(function (ns) {
                 this.b = d;
             }
 
-            ut.assert(base.isEqual(new A(1, 2), new B(1, 2)), "same object - different prototypes");
+            ut.assert(base.isEqual(new A(1, 2), new B(1, 2)),
+                      "same object - different prototypes");
         });
 
     }; // addTests
