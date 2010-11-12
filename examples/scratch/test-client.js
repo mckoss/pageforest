867fc293e9e2a68054f9d5861aa9c84a3caac182
Mike Koss <mckoss@startpad.org>
2010-11-12 11:03:13 -0800

    adding unit test framework to sample app

--- examples/scratch/test-client.js
+++ examples/scratch/test-client.js
@@ -1,10 +1,66 @@
 namespace.lookup('com.pageforest.client.test').defineOnce(function (ns) {
     var clientLib = namespace.lookup('com.pageforest.client');
 
+    var testBlob = {'testNum': 1,
+                    'testString': "hello",
+                    'testBool': false,
+                    'testObj': {'a': 1, 'b': 2},
+                    'testArray': [1, 2, 3]
+                   };
+
+    function TestApp() {
+    }
+
+    TestApp.methods({
+        getDoc: function() {
+            return {blob: testBlob};
+        },
+
+        setDoc: function(json) {
+            this.restore = json;
+        }
+    });
+
     function addTests(ts) {
 
+        ts.addTest("save/load", function(ut) {
+            var app = new TestApp();
+            var client = new clientLib.Client(app);
+
+            // Force a login check.
+            client.poll();
+
+            ut.asyncSequence([
+                function (ut) {
+                    client.onUserChange = function(username) {
+                        client.onUserChange = undefined;
+                        ut.assertType(username, 'string');
+                        client.save(testBlob, 'test-1');
+                        ut.nextFn();
+                    };
+                },
+
+                function (ut) {
+                    client.onStateChange = function(newState) {
+                        ut.assertEq(newState, 'clean');
+                        client.load('test-1');
+                        ut.nextFn();
+                    };
+                },
+
+                function (ut) {
+                    client.onStateChange = function(newState) {
+                        ut.assertEq(newState, 'loaded');
+                        ut.nextFn();
+                    };
+                }
+
+            ]);
+        }).async(true);
+
         ts.addTest("Client UI", function(ut) {
-            var client = new clientLib.Client(ns);
+            var app = new TestApp();
+            var client = new clientLib.Client(app);
 
             // Use the standard Pageforest UI widget.
             client.addAppBar();
