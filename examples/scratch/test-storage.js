e98c99c4ecc6ae0f143dae313cae833fb20af34e
Mike Koss <mckoss@startpad.org>
2010-11-15 14:20:38 -0800

    working on unit test of storage

--- examples/scratch/test-storage.js
+++ examples/scratch/test-storage.js
@@ -2,9 +2,17 @@ namespace.lookup('com.pageforest.storage.test').defineOnce(function (ns) {
     var clientLib = namespace.lookup('com.pageforest.client');
     var storage = namespace.lookup('com.pageforest.storage');
 
+    //var client = new clientLib.Client();
+
     function addTests(ts) {
         ts.addTest("putDoc", function(ut) {
-        });
+            ut.asyncSequence([
+                function (ut) {
+                    ut.nextFn();
+                }
+            ]);
+
+        }).async();
     }
 
     ns.addTests = addTests;
