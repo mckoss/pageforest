4c7a3c1b31543c824ea1d6ecb0221f97b4995eb9
Mike Koss <mckoss@startpad.org>
2010-12-17 15:43:03 -0800

    cleanup debug messages

--- appengine/urls.py
+++ appengine/urls.py
@@ -35,7 +35,7 @@ urlpatterns = patterns(
 
     # Static hosting for Pageforest apps.
     (r'^app/admin/app.json/$', 'apps.views.app_json'),
-    # The docid will be empty - key is path to an app blob
+    # Static app blob resources (files) - docid will be empty
     (r'^app/(admin/)?(?P<doc_id>)(?P<key>.*)$',
      'blobs.views.dispatch'),
 
