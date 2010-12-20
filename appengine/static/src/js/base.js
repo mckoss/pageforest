bda809e2bd0cb1231759051693eb22638bdb3105
Mike Koss <mckoss@startpad.org>
2010-12-19 16:21:48 -0800

    dictFromArray

--- appengine/static/src/js/base.js
+++ appengine/static/src/js/base.js
@@ -162,7 +162,7 @@ namespace.lookup('org.startpad.base').defineOnce(function(ns) {
     // Undefined properties are treated the same as un-set properties
     // in both Arrays and Objects.
     // Note that two objects with the same OWN properties can be equal
-    // if if they have different prototype chains (and inherited values).
+    // if they have different prototype chains (and inherited values).
     function isEqual(a, b) {
         if (a === b) {
             return true;
@@ -308,6 +308,17 @@ namespace.lookup('org.startpad.base').defineOnce(function(ns) {
         }
     }
 
+    function dictFromArray(a, keyName) {
+        var d = {};
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] === undefined || !(keyName in a[i])) {
+                continue;
+            }
+            d[a[i][keyName]] = a[i];
+        }
+        return d;
+    }
+
     // TODO: Use native implementations where available
     // in Array.prototype: map, reduce, filter, every, some,
     // indexOf, lastIndexOf.
@@ -332,7 +343,8 @@ namespace.lookup('org.startpad.base').defineOnce(function(ns) {
         'keys': keys,
         'forEach': forEach,
         'ensureArray': ensureArray,
-        'isEqual': isEqual
+        'isEqual': isEqual,
+        'dictFromArray': dictFromArray
     });
 
 }); // startpad.base
