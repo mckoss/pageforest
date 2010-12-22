94bef4b26fde027d48a23871e5a65dd148512579
Mike Koss <mckoss@startpad.org>
2010-12-22 09:50:07 -0800

    allow debug option

--- tools/test.py
+++ tools/test.py
@@ -3,6 +3,11 @@
 import os
 import sys
 
+DEBUG_MODULE = "pdb"
+#DEBUG_MODULE = "pudb.run"
+
+from optparse import OptionParser
+
 import pftool
 
 
@@ -14,10 +19,21 @@ def find_in_path(file_name):
             return '"%s"' % file_path
     return '"%s"' % file_name
 
+
 if __name__ == '__main__':
-    command = ['python2.5',
-               find_in_path('dev_appserver.py'),
-               '-a pageforest --show_mail_body --use_sqlite']
+    parser = OptionParser()
+    parser.add_option('-d', '--debug', action="store_true",
+                      help="Run app using pudb debugger.")
+    parser.add_option('-c', '--clear', action='store_true',
+                       help="Empty the data store before start.")
+    (options, args) = parser.parse_args()
+    command = ['python2.5']
+    if options.debug:
+        command.append('-m ' + DEBUG_MODULE)
+    command.extend((find_in_path('dev_appserver.py'),
+                    '-a pageforest --show_mail_body --use_sqlite'))
+    if options.clear:
+        command.append('--clear_datastore')
     command.extend(sys.argv[1:])
     command = ' '.join(command)
     if 'HOME' in os.environ:
