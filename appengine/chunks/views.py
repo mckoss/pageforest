1377b94aa798abcdadb5b31a5c026c698e0e6172
Mike Koss <mckoss@startpad.org>
2010-12-23 09:55:31 -0800

    Fix vacuum to also remove cache - also recover cached chunks.

--- appengine/chunks/views.py
+++ appengine/chunks/views.py
@@ -60,25 +60,45 @@ def vacuum(request, start):
             # This Blob doesn't reference a Chunk.
             blob = blobs.pop(0)
             if blob.size > MAX_INTERNAL_SIZE:
-                logging.error("Blob missing chunk: %s (%s)", (blob.key().name(), blob.sha1))
+                logging.error("Blob missing chunk: %s (%s)" % (blob.key().name(), blob.sha1))
                 blobs_without_chunks.append(blob)
         elif chunks[0].name() < blobs[0].sha1:
             # This Chunk is not referenced by any Blobs.
             chunks_without_blobs.append(chunks.pop(0))
             if len(chunks_without_blobs) >= MAX_VACUUM_CHUNKS:
                 break
+
+    do_action = ('HTTP_X_APPENGINE_CRON' in request.META or
+                 'confirmed' in request.POST)
+
     # Delete unreferenced chunks from the datastore.
-    if chunks_without_blobs and ('HTTP_X_APPENGINE_CRON' in request.META or
-                                 'confirmed' in request.POST):
-        db.delete(chunks_without_blobs)
-        logging.warning('deleted %d unreferenced chunks from %s to %s' % (
+    if chunks_without_blobs and do_action:
+        # MUST delete memcache keys as well as the store or else
+        # subsequent put's will leave dangline Blobs.
+        # BAD: db.delete(chunks_without_blobs)
+        Chunk.delete_keys(chunks_without_blobs)
+        logging.warning('Deleted %d unreferenced chunks from %s to %s' % (
                 len(chunks_without_blobs),
                 chunks_without_blobs[0].name(),
                 chunks_without_blobs[-1].name()))
         if request.method == 'POST':
             return HttpResponseRedirect(reverse(vacuum, args=[start]))
+
+    # ERROR!  Attempt to recover lost Chunk values from memcache.
+    recovered_chunks = []
+    if blobs_without_chunks:
+        chunks = Chunk.get_by_key_name_list([blob.sha1 for blob in blobs_without_chunks])
+        logging.info("chunks: %r" % chunks)
+        for chunk in chunks:
+            if chunk is None:
+                continue
+            chunk.put(write_through=True)
+            logging.warning("Recovered lost chunk: %s" % chunk.key().name())
+            recovered_chunks.append(chunk.key().name())
+
     return render_to_response(request, 'chunks/vacuum.html', {
             'chunks': original_chunks,
             'blobs': original_blobs,
             'blobs_without_chunks': blobs_without_chunks,
-            'chunks_without_blobs': chunks_without_blobs})
+            'chunks_without_blobs': chunks_without_blobs,
+            'recovered_chunks': recovered_chunks})
