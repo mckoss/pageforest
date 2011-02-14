8312ab9eaff0ca883862c8ba9c3e2f92d17a0198
Mike Koss <mckoss@startpad.org>
2011-02-14 14:01:30 -0800

    checkpoint - fix decorator init state and test

--- appengine/static/src/js/tests/test-debug.js
+++ appengine/static/src/js/tests/test-debug.js
@@ -5,6 +5,22 @@ namespace.lookup('org.startpad.debug.test').defineOnce(function (ns) {
     var debug = namespace.lookup('org.startpad.debug');
 
     ns.addTests = function (ts) {
+        function MockLogger(ut) {
+            this.ut = ut;
+        }
+
+        MockLogger.methods({
+            expect: function (s) {
+                this.expected = s;
+            },
+
+            log: function (s) {
+                if (this.expected) {
+                    this.ut.assertEq(s, this.expected);
+                    this.expected = undefined;
+                }
+            }
+        });
 
         function sample(a, b) {
         }
@@ -12,6 +28,15 @@ namespace.lookup('org.startpad.debug.test').defineOnce(function (ns) {
         ts.addTest("getFunctionName", function(ut) {
             ut.assertEq(debug.getFunctionName(sample), "sample");
             ut.assertEq(debug.getFunctionName(function () {}), "anonymous");
+        });
+
+        ts.addTest("alias", function(ut) {
+            var mockLogger = new MockLogger(ut);
+            debug.setLogger(mockLogger);
+
+            var s2 = sample.decorate(debug.alias, "aliasFunction");
+            mockLogger.expect("aliasFunction is deprecated - use sample instead.");
+            s2();
         });
     };
 
