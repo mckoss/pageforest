86f19a9848fd939abe376d3f883dd0082710bd37
Mike Koss <mckoss@startpad.org>
2011-02-15 14:22:45 -0800

    checkpoint - allow access

--- appengine/static/src/js/dialog.js
+++ appengine/static/src/js/dialog.js
@@ -29,8 +29,8 @@ namespace.lookup('org.startpad.dialog').defineOnce(function(ns) {
             pre: '',
             label: '<label class="left" for="{id}">{label}:</label>',
             content: '<input id="{id}" type="text"/>',
-            spanRow: '{content}\n',
-            row: "{label}{content}\n",
+            spanRow: '<div id ="{id}-row">{content}</div>\n',
+            row: '<div id="{id}-row">{label}{content}</div>\n',
             post: '<div style="clear: both;"></div>\n',
             dialogClass: 'sp-dialog-div'
         },
@@ -38,8 +38,8 @@ namespace.lookup('org.startpad.dialog').defineOnce(function(ns) {
             pre: "<table>\n",
             label: '<label class="left" for="{id}">{label}:</label>',
             content: '<input id="{id}" type="text"/>',
-            spanRow: "<tr><td colspan=2>{content}</td></tr>",
-            row: "<tr><th>{label}</th><td>{content}</td></tr>\n",
+            spanRow: '<tr id="{id}-row"><td colspan=2>{content}</td></tr>',
+            row: '<tr id="{id}-row"><th>{label}</th><td>{content}</td></tr>\n',
             post: "</table>\n",
             dialogClass: 'sp-dialog-table'
         }
@@ -97,7 +97,8 @@ namespace.lookup('org.startpad.dialog').defineOnce(function(ns) {
                 }
                 var fieldPatterns = base.extendIfMissing({}, self.patterns[field.type],
                         base.project(self.style, ['label', 'content']));
-                var row = {label: fieldPatterns.label.format(field),
+                var row = {id: field.id,
+                           label: fieldPatterns.label.format(field),
                            content: fieldPatterns.content.format(field)};
                 var rowPattern = self[fieldPatterns.useRow] || self.row;
                 stb.append(rowPattern.format(row));
@@ -173,6 +174,7 @@ namespace.lookup('org.startpad.dialog').defineOnce(function(ns) {
         },
 
         getField: function(name) {
+            this.bindFields();
             for (var i = 0; i < this.fields.length; i++) {
                 if (this.fields[i].name == name) {
                     return this.fields[i];
@@ -195,7 +197,6 @@ namespace.lookup('org.startpad.dialog').defineOnce(function(ns) {
 
             base.extendObject(this.lastValues, values);
 
-            this.bindFields();
             for (var name in values) {
                 if (values.hasOwnProperty(name)) {
                     field = this.getField(name);
@@ -283,11 +284,18 @@ namespace.lookup('org.startpad.dialog').defineOnce(function(ns) {
             return values;
         },
 
+        showField: function(name, shown) {
+            if (shown == undefined) {
+                shown = true;
+            }
+            var field = this.getField(name);
+            $('#' + field.id + '-row')[shown ? 'show' : 'hide']();
+        },
+
         enableField: function(name, enabled) {
             if (enabled == undefined) {
                 enabled = true;
             }
-            this.bindFields();
             var field = this.getField(name);
             switch (field.elt.tagName) {
             case 'INPUT':
