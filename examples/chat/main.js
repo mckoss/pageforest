b772551d185819274fa2ff5e783300b1ad0e69da
Mike Koss <mckoss@startpad.org>
2011-02-28 16:44:55 -0800

    updated to use channel

--- examples/chat/main.js
+++ examples/chat/main.js
@@ -1,114 +1,83 @@
 namespace.lookup('com.pageforest.chat').defineOnce(function (ns) {
+    var clientLib = namespace.lookup('com.pageforest.client');
+    var dom = namespace.lookup('org.startpad.dom');
+    var client;
 
-    // Long polling for new chat messages from remote users.
-    function poll(wait) {
-        if (!ns.client.docid) {
-            return; // We have not entered a chatroom yet.
-        }
-        if (ns.xhr) {
-            return; // The previous poll is still in progress.
-        }
-        ns.xhr = $.ajax({
-            url: '/docs/' + ns.client.docid + '/messages?wait=' + wait,
-            dataType: 'json',
-            ifModified: true,
-            success: function(history, status, xhr) {
-                $('#status').text(
-                    xhr.status + ' ' + xhr.statusText + ' ' + status);
-                if (xhr.status == 200) {
-                    var textarea = $('#messages');
-                    textarea.val(history.join('\n'));
-                    textarea.attr('scrollTop', textarea.attr('scrollHeight'));
-                }
-            },
-            complete: function() {
-                ns.xhr = false;
-            }
-        });
-    }
-
-    function comet() {
-        poll(5);
-    }
+    var doc;
+    var roomid = 'entry';
+    var lines = [];
 
     // Initialize the document - create a client helper object.
     function onReady() {
-        var clientLib = namespace.lookup('com.pageforest.client');
-        ns.client = new clientLib.Client(ns);
-        ns.client.setLogging(true);
+        doc = dom.bindIDs();
+        client = new clientLib.Client(ns);
+        client.addAppBar();
+
         $('#docid').focus();
-        // Start a new long poll request whenever necessary.
-        setInterval(comet, 1000);
-    }
-
-    // Called on any api errors.
-    function onError(status, message) {
-        $('#status').text(status + ' ' + message);
-    }
-
-    // Called when the current user changes (signs in or out)
-    function onUserChange(username) {
-        $('#username').text(username ? username : 'anonymous');
-        $('#signin').val(username ? 'Sign Out' : 'Sign In');
-    }
-
-    // Sign in (or out) depending on current user state.
-    function signInOut() {
-        if (ns.client.username) {
-            ns.client.signOut();
-        } else {
-            ns.client.signIn();
-        }
     }
 
     // Retrieve the current document state.
     function getDoc() {
         return {
-            title: "Public chat",
-            blob: "blob", // FIXME optional.
+            blob: "Nothing here - see message blob.",
             readers: ["public"],
-            writers: ["authenticated"]
+            writers: ["public"]
         };
+    }
+
+    function onMessage(a) {
+        for (var i = 0; i < a.length; i++) {
+            lines.push(a[i].username + ': ' + a[i].message);
+        }
+        $('#messages').val(lines.join('\n'));
+        dom.scrollToBottom(doc.messages);
+    }
+
+    function onSubscribe(message) {
+        client.storage.slice(roomid, 'messages', {start: -1}, onMessage);
     }
 
     // Respond to document load success.
     function setDoc(doc) {
-        $('#status').html('Joined chatroom: ' + ns.client.docid);
-        poll(0);
+        $('#status').html('Joined chatroom: ' + client.docid);
+        roomid = client.docid;
+        $('#roomid').val(roomid);
+        client.storage.subscribe(roomid, 'messages', {exclusive: true}, onSubscribe);
+        client.storage.getBlob(roomid, 'messages', undefined, onMessage);
+
+
+        $('#document-data').attr('href', '/docs/' + roomid);
+        $('#message-data').attr('href', '/docs/' + roomid + '/messages');
+    }
+
+    function getDocid() {
+        return roomid;
     }
 
     // Join a chatroom by name.
     // This creates the chatroom if it didn't exist already.
     function join() {
-        ns.client.save(undefined, $('#docid').val());
+        roomid = $('#roomid').val();
+        return false;
     }
 
     // Send a message to the chat.
     function submit() {
-        $.ajax({
-            type: 'POST',
-            url: '/docs/chat/messages?method=push&max=50',
-            data: ns.client.username + ': ' + $('#message').val(),
-            dataType: 'text',
-            success: function(message, status, xhr) {
-                $('#status').html(message);
-            },
-            error: function(xhr, status, message) {
-                $('#status').html(status + " " + message);
-            }
-        });
-        $('#message').val('');
-        $('#message').focus();
+        var data = {
+            username: client.username,
+            message: $('#message').val()
+        };
+        client.storage.push(roomid, 'messages', data);
+        $('#message').val('').focus();
+        return false;
     }
 
     // Exported functions
     ns.extend({
         onReady: onReady,
-        onError: onError,
-        onUserChange: onUserChange,
-        signInOut: signInOut,
         getDoc: getDoc,
         setDoc: setDoc,
+        getDocid: getDocid,
         join: join,
         submit: submit
     });
