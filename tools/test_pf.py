378cbd0bdaaea7d515c1c3a25d02d5a13f787f69
Mike Koss <mckoss@startpad.org>
2011-03-05 18:31:05 -0800

    checkpoint - pf.py test

--- tools/test_pf.py
+++ tools/test_pf.py
@@ -4,10 +4,15 @@ Test the Pageforest Application Uploader - pf.py.
 """
 
 SERVER = 'pageforest.com'
-TEST_APP = 'test_pf'
+TEST_APP = 'pfpytest'
 
 import unittest
 import pf
+
+
+class TestAuthenticate(unittest.TestCase):
+
+    def setUp(self):
 
 if __name__ == '__main__':
     unittest.main()
