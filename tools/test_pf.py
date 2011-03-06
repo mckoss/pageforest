70cc3c9f7cf5d00b5ca7c0f6c0bd961dea65d993
Mike Koss <mckoss@startpad.org>
2011-03-05 18:19:33 -0800

    Start independent pf.py unit test suite.

---
+++ tools/test_pf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+Test the Pageforest Application Uploader - pf.py.
+"""
+
+SERVER = 'pageforest.com'
+TEST_APP = 'test_pf'
+
+import unittest
+import pf
+
+if __name__ == '__main__':
+    unittest.main()
