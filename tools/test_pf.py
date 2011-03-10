5cfe7cdc1e9aaef64552bf01026991d6dd7084cc
Mike Koss <mckoss@startpad.org>
2011-03-09 18:37:29 -0800

    put test

--- tools/test_pf.py
+++ tools/test_pf.py
@@ -7,6 +7,7 @@ import shlex
 import subprocess
 import shutil
 import unittest
+from random import random
 
 import pf
 
@@ -23,16 +24,6 @@ test_dir = os.path.join(tools_dir, TEST_APP)
 pf_cmd = os.path.join(tools_dir, 'pf.py')
 
 """
-def test_command(args):
-    # Create temp folder.
-    dirname = options.application
-    if not os.path.exists(dirname):
-        os.mkdir(dirname)
-    os.chdir(dirname)
-    # Create META_FILENAME with metadata.
-    outfile = open(META_FILENAME, 'w')
-    outfile.write('{"application": "%s"}' % options.application)
-    outfile.close()
     # Create test.txt with current timestamp as content.
     write_data = datetime.now().isoformat()
     filename = 'test.txt'
@@ -75,20 +66,31 @@ def shell_command(command_line):
     return (proc.returncode, out)
 
 
-def assert_command(test, command_line, contains="", expect_code=0):
+def assert_command(test, command_line, contains=None, not_contains=None, expect_code=0):
     (code, out) = shell_command(command_line)
 
     test.assertEqual(code, expect_code,
                      "Unexpected exit code, %d from '%s'" % (code, command_line))
-    if contains:
-        if type(contains) not in (list, tuple):
-            contains = (contains,)
+
+    def list_default(param):
+        if param is None:
+            return ()
+        if isinstance(param, basestring):
+            return (param,)
+        return param
+
+    contains = list_default(contains)
+    not_contains = list_default(not_contains)
 
     for pattern in contains:
         test.assertNotEqual(out.find(pattern), -1,
                             "Missing '%s' from '%s'.\n---\n%s\n---" %
                             (pattern, command_line, out))
 
+    for pattern in not_contains:
+        test.assertEqual(out.find(pattern), -1,
+                            "Should not have '%s' from '%s'.\n---\n%s\n---" %
+                            (pattern, command_line, out))
     return out
 
 
@@ -127,7 +129,7 @@ class TestLocal(TestPF):
     def test_dir(self):
         out = assert_command(self, pf_cmd + ' dir',
                              contains=['1 file',
-                                       '5f36b2ea290645ee34d943220a14b54ee5ea5be5'
+                                       '5f36b2ea290645ee34d943220a14b54ee5ea5be5',
                                        ])
 
     def test_offline(self):
@@ -143,8 +145,27 @@ class TestAuth(TestPF):
                                  TEST_APP,
                                  ])
 
+
 class TestPut(TestPF):
-    pass
+    def setUp(self):
+        super(TestPut, self).setUp()
+        make_file('test.txt', "This is a text file\n")
+        make_file('unique.txt', str(random()))
+
+    def test_dir(self):
+        assert_command(self, pf_cmd + ' dir',
+                       contains=['3 files',
+                                 '202712ad248cc7617ffdcc6991358bf98debcb25',
+                                 ])
+
+    def test_put(self):
+        assert_command(self, pf_cmd + ' put',
+                       contains=['Uploading',
+                                 'http://admin.test-pf.',
+                                 'app.json',
+                                 'unique.txt'])
+        assert_command(self, pf_cmd + ' put',
+                       not_contains='unique.txt')
 
 if __name__ == '__main__':
     unittest.main()
